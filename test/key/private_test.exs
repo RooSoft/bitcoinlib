e55b9f45781655d38bf7c49ed9084d173936b409
RooSoft <marc@roosoft.com>
2022-06-01 14:32:28 -0400

    fixed creates a WIF from a private key test

--- test/key/private_test.exs
+++ test/key/private_test.exs
@@ -4,7 +4,7 @@ defmodule BitcoinLib.Key.PrivateTest do
   alias BitcoinLib.Key.Private
 
   test "creates a WIF from a private key" do
-    private_key = "6C7AB2F961A1BC3F13CDC08DC41C3F439ADEBD549A8EF1C089E81A5907376107"
+    private_key = 0x6C7AB2F961A1BC3F13CDC08DC41C3F439ADEBD549A8EF1C089E81A5907376107
 
     wif =
       private_key
