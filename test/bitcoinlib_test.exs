fa945c68dfa21945a86d578040a491615de57cd7
RooSoft <marc@roosoft.com>
2022-06-01 08:06:05 -0400

    test generate a private key

--- test/bitcoinlib_test.exs
+++ test/bitcoinlib_test.exs
@@ -1,8 +1,10 @@
-defmodule BitcoinLib.CryptoTest do
+defmodule BitcoinLib.Test do
   use ExUnit.Case
 
   test "generate a private key" do
-    BitcoinLib.
-  end
+    private_key = BitcoinLib.generate_private_key()
 
+    assert is_bitstring(private_key)
+    assert byte_size(private_key) == 32
+  end
 end
