8e737b421d2537f5654d7c39362a3aa90aaaddda
RooSoft <marc@roosoft.com>
2022-06-01 14:39:50 -0400

    added public key derivation from a private key test in bitcoinlib_test

--- test/bitcoinlib_test.exs
+++ test/bitcoinlib_test.exs
@@ -14,13 +14,16 @@ defmodule BitcoinLib.Test do
     assert byte_size(private_key) == 52
   end
 
-  # test "public key derivation from a private key" do
-  #   private_key =
-  #     <<189, 95, 7, 16, 129, 49, 152, 91, 12, 2, 172, 172, 1, 37, 219, 223, 174, 232, 13, 196,
-  #       196, 117, 255, 172, 182, 94, 51, 46, 48, 207, 238, 36>>
+  test "public key derivation from a private key" do
+    private_key = "0A8D286B11B98F6CB2585B627FF44D12059560ACD430DCFA1260EF2BD9569373"
 
-  #   public_key = BitcoinLib.derive_public_key()
+    {uncompressed, compressed} =
+      private_key
+      |> BitcoinLib.derive_public_key()
 
-  #   assert true
-  # end
+    assert uncompressed ==
+             "040F69EF8F2FEB09B29393EEF514761F22636B90D8E4D3F2138B2373BD37523053002119E16B613619691F760EADD486315FC9E36491C7ADB76998D1B903B3DD12"
+
+    assert compressed == "020F69EF8F2FEB09B29393EEF514761F22636B90D8E4D3F2138B2373BD37523053"
+  end
 end
