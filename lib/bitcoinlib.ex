2f90ed8ded73b6fde18fd993b3806719373855af
RooSoft <marc@roosoft.com>
2022-06-02 12:52:25 -0400

    made a test for BitcoinLib.derive_public_key

--- lib/bitcoinlib.ex
+++ lib/bitcoinlib.ex
@@ -16,6 +16,17 @@ defmodule BitcoinLib do
     Private.generate()
   end
 
+  @doc """
+  Derives a public key from a raw private key
+
+  ## Examples
+
+    iex> "0a8d286b11b98f6cb2585b627ff44d12059560acd430dcfa1260ef2bd9569373" |> BitcoinLib.derive_public_key()
+    {
+      "040f69ef8f2feb09b29393eef514761f22636b90d8e4d3f2138b2373bd37523053002119e16b613619691f760eadd486315fc9e36491c7adb76998d1b903b3dd12",
+      "020f69ef8f2feb09b29393eef514761f22636b90d8e4d3f2138b2373bd37523053"
+    }
+  """
   def derive_public_key(private_key) do
     Public.from_private_key(private_key)
   end
