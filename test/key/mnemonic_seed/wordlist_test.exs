00e16a69a553460b11003a612dbf8bb436295580
RooSoft <marc@roosoft.com>
2022-06-06 10:52:38 -0400

    added a test for a 12 word mnemonic

--- test/key/mnemonic_seed/wordlist_test.exs
+++ test/key/mnemonic_seed/wordlist_test.exs
@@ -27,4 +27,17 @@ defmodule BitcoinLib.Key.MnemonicSeed.WordlistTest do
              "toddler"
            ]
   end
+
+  test "convert another 128 bit seed into a 12 words list" do
+    seed = 36_783_498_498_455_780_461_879_399_537_283_362_692
+
+    wordlist =
+      seed
+      |> Wordlist.convert_seed()
+
+    assert wordlist ==
+             String.split(
+               "brick giggle panic mammal document foam gym canvas wheel among room analyst"
+             )
+  end
 end
