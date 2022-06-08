bc4ea852b33e5fccd8f08b9c2c4fbffabbc505eb
RooSoft <marc@roosoft.com>
2022-06-08 11:35:13 -0400

    documented and specd BitUtils.combine

--- lib/crypto/bit_utils.ex
+++ lib/crypto/bit_utils.ex
@@ -23,6 +23,15 @@ defmodule BitcoinLib.Crypto.BitUtils do
     split(binary, bit_size, [])
   end
 
+  @doc """
+  Regroups a list of binaries into a single one
+
+  ## Examples
+    iex> [<<6, 6::size(3)>>, <<61, 2::size(3)>>, <<203, 7::size(3)>>, <<44, 1::size(3)>>, <<10::size(4)>>]
+    ...> |> BitcoinLib.Crypto.BitUtils.combine
+    <<6, 199, 171, 47, 150, 26>>
+  """
+  @spec combine(list(Binary.t())) :: Binary.t()
   def combine(binary_list) do
     binary_list
     |> Enum.reduce(<<>>, fn binary, acc ->
