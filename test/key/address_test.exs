b15c59ae08422f5ba71cd3f219c65fe61a6ed459
RooSoft <marc@roosoft.com>
2022-06-09 09:08:23 -0400

    test to make sure default addresses are P2SH

--- test/key/address_test.exs
+++ test/key/address_test.exs
@@ -24,4 +24,14 @@ defmodule BitcoinLib.Key.AddressTest do
 
     assert address == "3BSAJ2tDV6bcv38x3SmCo3Z3nPqWHYxcM9"
   end
+
+  test "make sure default addresses are P2SH" do
+    public_key_hash = "6ae201797de3fa7d1d95510f50c1a9c50ce4cc36"
+
+    address =
+      public_key_hash
+      |> Address.from_public_key_hash()
+
+    assert address == "3BSAJ2tDV6bcv38x3SmCo3Z3nPqWHYxcM9"
+  end
 end
