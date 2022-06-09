2cc923a658ab64fdfaf17662939e24976eab6ada
RooSoft <marc@roosoft.com>
2022-06-09 09:07:46 -0400

    test P2SH addresses

--- test/key/address_test.exs
+++ test/key/address_test.exs
@@ -14,4 +14,14 @@ defmodule BitcoinLib.Key.AddressTest do
 
     assert address == "1Ak9NVPmwCHEpsSWvM6cNRC7dsYniRmwMG"
   end
+
+  test "generate a P2SH from a public key hash" do
+    public_key_hash = "6ae201797de3fa7d1d95510f50c1a9c50ce4cc36"
+
+    address =
+      public_key_hash
+      |> Address.from_public_key_hash(:p2sh)
+
+    assert address == "3BSAJ2tDV6bcv38x3SmCo3Z3nPqWHYxcM9"
+  end
 end
