a08f7eb193093145f683222b4853dff10d6af5ce
RooSoft <marc@roosoft.com>
2022-06-14 15:11:10 -0400

    fixed iex.exs to use private keys in integer format

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,4 @@
 mnemonics = "brick giggle panic mammal document foam gym canvas wheel among room analyst"
 seed = BitcoinLib.Key.HD.MnemonicSeed.to_seed(mnemonics)
 private_key = BitcoinLib.Key.HD.ExtendedPrivate.from_seed(seed)
-public_key = BitcoinLib.Key.HD.ExtendedPublic.from_private_key(private_key.key |> Binary.to_hex)
+public_key = BitcoinLib.Key.HD.ExtendedPublic.from_private_key(private_key.key)
