a1e3e63dc7fb14f9fdf0f3ce77edc7650f96e9ad
RooSoft <marc@roosoft.com>
2022-06-16 13:35:05 -0400

    updated .iex.exs to create everyting up to a public key from dice rolls

--- .iex.exs
+++ .iex.exs
@@ -1,4 +1,6 @@
-mnemonics = "brick giggle panic mammal document foam gym canvas wheel among room analyst"
+dice_rolls = "01234501234501234501234501234501234501234501234501"
+{:ok, entropy} = BitcoinLib.Key.HD.Entropy.from_dice_rolls(dice_rolls)
+mnemonics = BitcoinLib.Key.HD.MnemonicSeed.wordlist_from_entropy(entropy)
 seed = BitcoinLib.Key.HD.MnemonicSeed.to_seed(mnemonics)
 private_key = BitcoinLib.Key.HD.ExtendedPrivate.from_seed(seed)
 public_key = BitcoinLib.Key.HD.ExtendedPublic.from_private_key(private_key.key)
