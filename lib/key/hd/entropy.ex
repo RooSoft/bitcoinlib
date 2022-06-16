3265f03599614baad2c203ca126ab963ff6fecf1
RooSoft <marc@roosoft.com>
2022-06-16 13:29:11 -0400

    validate dice rolls before converting them into entropy

--- lib/key/hd/entropy.ex
+++ lib/key/hd/entropy.ex
@@ -11,12 +11,33 @@ defmodule BitcoinLib.Key.HD.Entropy do
   ## Examples
     iex> "012345012345012345012345012345012345012345012345012"
     ...> |> BitcoinLib.Key.HD.Entropy.from_dice_rolls()
+    ...> |> elem(1)
     193_862_769_152_946_304_546_066_490_817_889_639_328
   """
-  @spec from_dice_rolls(String.t()) :: Integer.t()
+  @spec from_dice_rolls(String.t()) :: {:ok, Integer.t()} | {:error, String.t()}
   def from_dice_rolls(dice_rolls) do
     dice_rolls
-    |> Integer.parse(6)
-    |> elem(0)
+    |> validate_rolls
+    |> maybe_parse_rolls
+  end
+
+  defp validate_rolls(dice_rolls) do
+    {
+      Regex.match?(~r/^[0-5]+$/, dice_rolls),
+      dice_rolls
+    }
+  end
+
+  defp maybe_parse_rolls({true, dice_rolls}) do
+    entropy =
+      dice_rolls
+      |> Integer.parse(6)
+      |> elem(0)
+
+    {:ok, entropy}
+  end
+
+  defp maybe_parse_rolls({false, _}) do
+    {:error, "dice_rolls contains invalid characters outside of this range [0,5]"}
   end
 end
--- test/key/hd/entropy_test.exs
+++ test/key/hd/entropy_test.exs
@@ -8,7 +8,7 @@ defmodule BitcoinLib.Key.HD.EntropyTest do
   test "create an entropy value out of 50 dice rolls" do
     dice_rolls = "012345012345012345012345012345012345012345012345012"
 
-    entropy = Entropy.from_dice_rolls(dice_rolls)
+    {:ok, entropy} = Entropy.from_dice_rolls(dice_rolls)
 
     assert entropy == 193_862_769_152_946_304_546_066_490_817_889_639_328
   end
