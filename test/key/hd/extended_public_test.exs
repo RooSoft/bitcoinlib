e567cc9a54992b798cb325661f7912aa581c23cd
RooSoft <marc@roosoft.com>
2022-06-17 14:26:49 -0400

    test serialize a master public key

--- test/key/hd/extended_public_test.exs
+++ test/key/hd/extended_public_test.exs
@@ -17,4 +17,15 @@ defmodule BitcoinLib.Key.HD.ExtendedPublicTest do
     assert 0x0443B337DEC65A47B3362C9620A6E6FF39A1DDFA908ABAB1666C8A30A3F8A7CCCCFC24A7914950B6405729A9313CEC6AE5BB4A082F92D05AC49DF4B6DD8387BFEB ==
              uncompressed
   end
+
+  test "serialize a master public key" do
+    primary_key = 0x339A36013301597DAEF41FBE593A02CC513D0B55527EC2DF1050E2E8FF49C85C2
+    chain_code = 0x873DFF81C02F525623FD1FE5167EAC3A55A049DE3D314BB42EE227FFED37D508
+
+    serialized =
+      BitcoinLib.Key.HD.ExtendedPublic.serialize_master_public_key(primary_key, chain_code)
+
+    assert "xpub661MyMwAqRbcFtXgS5sYJABqqG9YLmC4Q1Rdap9gSE8NqtwybGhePY2gZ29ESFjqJoCu1Rupje8YtGqsefD265TMg7usUDFdp6W1EGMcet8" ==
+      serialized
+  end
 end
