a62ed052b6f19bfeaa11996ebaf1edef90d1c7e0
RooSoft <marc@roosoft.com>
2022-06-21 09:35:13 -0400

    console can now pretty print public keys

--- lib/console.ex
+++ lib/console.ex
@@ -1,14 +1,29 @@
 defmodule BitcoinLib.Console do
-  alias BitcoinLib.Key.HD.ExtendedPrivate
+  alias BitcoinLib.Key.HD.{ExtendedPrivate, ExtendedPublic}
 
   def write(%ExtendedPrivate{} = private_key) do
     tabulation = 12
+
+    print_header("PRIVATE_KEY", tabulation)
 
     print_with_title("key", tabulation, private_key.key, 64)
     print_with_title("chain_code", tabulation, private_key.chain_code, 64)
     print_with_title("depth", tabulation, private_key.depth)
     print_with_title("index", tabulation, private_key.index)
     print_with_title("fingerprint", tabulation, private_key.parent_fingerprint)
+  end
+
+  def write(%ExtendedPublic{} = public_key) do
+    tabulation = 12
+
+    IO.puts("PUBLIC KEY")
+    IO.puts("----------")
+
+    print_with_title("key", tabulation, public_key.key, 66)
+    print_with_title("chain_code", tabulation, public_key.chain_code, 64)
+    print_with_title("depth", tabulation, public_key.depth)
+    print_with_title("index", tabulation, public_key.index)
+    print_with_title("fingerprint", tabulation, public_key.parent_fingerprint)
   end
 
   defp to_fixed_hex_string(value, length) when is_integer(value) do
@@ -33,8 +48,22 @@ defmodule BitcoinLib.Console do
   end
 
   defp print_with_title(title, title_length, value) when is_binary(value) do
-    IO.puts(
-      "#{IO.ANSI.yellow()}#{title |> String.pad_leading(title_length, " ")}: #{IO.ANSI.default_color()}#{value}"
-    )
+    IO.puts("#{blue(title |> String.pad_leading(title_length, " "))}: #{yellow value}")
+  end
+
+  defp print_header(header, tabulation) do
+    header_length = String.length(header)
+    underline = "" |> String.pad_leading(header_length, "-")
+
+    IO.puts("#{"" |> String.pad_leading(tabulation + 2)}#{blue(header)}")
+    IO.puts("#{"" |> String.pad_leading(tabulation + 2)}#{underline}")
+  end
+
+  defp yellow(string) do
+    "#{IO.ANSI.yellow()}#{string}#{IO.ANSI.default_color()}"
+  end
+
+  defp blue(string) do
+    "#{IO.ANSI.blue()}#{string}#{IO.ANSI.default_color()}"
   end
 end
