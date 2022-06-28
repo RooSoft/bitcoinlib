d551662c5e31b4d7853ba2f2c24ea65e6fc6989d
RooSoft <marc@roosoft.com>
2022-06-28 13:47:12 -0400

    added derive_child and derive_child! to ExtendedPublic

--- test/key/hd/extended_public_test.exs
+++ test/key/hd/extended_public_test.exs
@@ -33,42 +33,44 @@ defmodule BitcoinLib.Key.HD.ExtendedPublicTest do
              serialized
   end
 
-  # test "derive the first child" do
-  #   public_key = %BitcoinLib.Key.HD.ExtendedPublic{
-  #     key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
-  #     chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
-  #   }
+  test "derive the first child" do
+    public_key = %BitcoinLib.Key.HD.ExtendedPublic{
+      key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
+      chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
+    }
 
-  #   index = 0
-  #   BitcoinLib.Key.HD.ExtendedPublic.derive_child(public_key, index)
+    index = 0
 
-  #   {
-  #     :ok,
-  #     %BitcoinLib.Key.HD.ExtendedPublic{
-  #       key: 0x951D9004DE141E9DC5FA045A15F3E71AE5592C2E5D74332CB4E034679950D0E1,
-  #       chain_code: 0x05AAE71D7C080474EFAAB01FA79E96F4C6CFE243237780B0DF4BC36106228E31,
-  #       depth: 1,
-  #       index: 0,
-  #       parent_fingerprint: 0x18C1259
-  #     }
-  #   }
-  # end
+    result = BitcoinLib.Key.HD.ExtendedPublic.derive_child(public_key, index)
 
-  # test "derive the first child, without error checking" do
-  #   public_key = %BitcoinLib.Key.HD.ExtendedPublic{
-  #     key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
-  #     chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
-  #   }
+    {
+      :ok,
+      %BitcoinLib.Key.HD.ExtendedPublic{
+        key: 0x30204D3503024160E8303C0042930EA92A9D671DE9AA139C1867353F6B6664E59,
+        chain_code: 0x05AAE71D7C080474EFAAB01FA79E96F4C6CFE243237780B0DF4BC36106228E31,
+        depth: 1,
+        index: 0,
+        parent_fingerprint: 0x18C1259
+      }
+    } == result
+  end
 
-  #   index = 0
-  #   BitcoinLib.Key.HD.ExtendedPublic.derive_child!(public_key, index)
+  test "derive the first child, without error checking" do
+    public_key = %BitcoinLib.Key.HD.ExtendedPublic{
+      key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
+      chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
+    }
 
-  #   %BitcoinLib.Key.HD.ExtendedPublic{
-  #     key: 0x951D9004DE141E9DC5FA045A15F3E71AE5592C2E5D74332CB4E034679950D0E1,
-  #     chain_code: 0x05AAE71D7C080474EFAAB01FA79E96F4C6CFE243237780B0DF4BC36106228E31,
-  #     depth: 1,
-  #     index: 0,
-  #     parent_fingerprint: 0x18C1259
-  #   }
-  # end
+    index = 0
+
+    child = BitcoinLib.Key.HD.ExtendedPublic.derive_child!(public_key, index)
+
+    assert %BitcoinLib.Key.HD.ExtendedPublic{
+      key: 0x30204D3503024160E8303C0042930EA92A9D671DE9AA139C1867353F6B6664E59,
+      chain_code: 0x05AAE71D7C080474EFAAB01FA79E96F4C6CFE243237780B0DF4BC36106228E31,
+      depth: 1,
+      index: 0,
+      parent_fingerprint: 0x18C1259
+    } == child
+  end
 end
