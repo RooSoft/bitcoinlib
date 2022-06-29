1ac5016080fdce295e5c56a9a28b0d268cc535b5
RooSoft <marc@roosoft.com>
2022-06-29 09:21:42 -0400

    added doctest for BitcoinLib.Key.HD.ExtendedPrivate.ChildFromDerivationPath.get

--- lib/key/hd/extended_private/child_from_derivation_path.ex
+++ lib/key/hd/extended_private/child_from_derivation_path.ex
@@ -1,8 +1,33 @@
 defmodule BitcoinLib.Key.HD.ExtendedPrivate.ChildFromDerivationPath do
+  @moduledoc """
+  Computes a private key from a derivation path
+  """
+
   alias BitcoinLib.Key.HD.{DerivationPath, ExtendedPrivate}
   alias BitcoinLib.Key.HD.DerivationPath.{Level}
   alias BitcoinLib.Key.HD.ExtendedPrivate.{ChildFromIndex}
 
+  @doc """
+  Computes a private key from a derivation path
+
+  ## Examples
+    iex> { :ok, derivation_path } = BitcoinLib.Key.HD.DerivationPath.parse("m/84'/0'/0'/0/0")
+    ...> %BitcoinLib.Key.HD.ExtendedPrivate{
+    ...>  key: 0xF79BB0D317B310B261A55A8AB393B4C8A1ABA6FA4D08AEF379CABA502D5D67F9,
+    ...>  chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
+    ...> }
+    ...> |> BitcoinLib.Key.HD.ExtendedPrivate.ChildFromDerivationPath.get(derivation_path)
+    {
+      :ok,
+      %BitcoinLib.Key.HD.ExtendedPrivate{
+        key: 0xF884F5D6A4CB4F2F859FBE1FE75DFE5A48822FD56A59A0B375FC0BAF8CC5C84E,
+        chain_code: 0xB0B26A820035421BD94DAE603F7354C275EDF007A4086BDD67B327301D852EBE,
+        depth: 4,
+        index: 0,
+        parent_fingerprint: 0x7C33408C
+        }
+      }
+  """
   @spec get(%ExtendedPrivate{}, %DerivationPath{}) :: {:ok, %ExtendedPrivate{}}
   def get(%ExtendedPrivate{} = private_key, %DerivationPath{} = derivation_path) do
     {child_private_key, _} =
