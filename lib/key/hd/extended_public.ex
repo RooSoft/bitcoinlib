b817d88193ceb89b7e52a8da14261415e5abd9c0
RooSoft <marc@roosoft.com>
2022-06-30 14:15:08 -0400

    added ExtendedPublic.from_derivation_path

--- lib/key/hd/extended_public.ex
+++ lib/key/hd/extended_public.ex
@@ -6,8 +6,14 @@ defmodule BitcoinLib.Key.HD.ExtendedPublic do
   @enforce_keys [:key, :chain_code]
   defstruct [:key, :chain_code, depth: 0, index: 0, parent_fingerprint: 0, fingerprint: 0]
 
-  alias BitcoinLib.Key.HD.ExtendedPublic.{ChildFromIndex, Serialization, Deserialization}
-  alias BitcoinLib.Key.HD.{ExtendedPrivate, ExtendedPublic, Fingerprint}
+  alias BitcoinLib.Key.HD.ExtendedPublic.{
+    ChildFromIndex,
+    ChildFromDerivationPath,
+    Serialization,
+    Deserialization
+  }
+
+  alias BitcoinLib.Key.HD.{DerivationPath, ExtendedPrivate, ExtendedPublic, Fingerprint}
 
   @doc """
   Derives an extended public key from an extended private key. Happens to be the same process
@@ -214,6 +220,32 @@ defmodule BitcoinLib.Key.HD.ExtendedPublic do
   def derive_child!(public_key, index) do
     derive_child(public_key, index)
     |> elem(1)
+  end
+
+  @doc """
+  Derives a child public key, following a derivation path
+
+  ## Examples
+    iex> public_key = %BitcoinLib.Key.HD.ExtendedPublic{
+    ...>   key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
+    ...>   chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
+    ...> }
+    ...> {:ok, derivation_path} = BitcoinLib.Key.HD.DerivationPath.parse("M/44'/0'/0'/0/0")
+    ...> BitcoinLib.Key.HD.ExtendedPublic.from_derivation_path(public_key, derivation_path)
+    {
+      :ok,
+      %BitcoinLib.Key.HD.ExtendedPublic{
+        key: 0x29DCAFD0D7D67B13657CC9EE7C8976E141F20F0684BF3FC83CAF068E74186BCDC,
+        chain_code: 0x162EEE68F7C3823CAF8BD2615A4A33633673CAAB66FF6F338FB0653FC59D462D,
+        depth: 5,
+        index: 0,
+        parent_fingerprint: 0xCA2A5281
+      }
+    }
+  """
+  @spec from_derivation_path(%ExtendedPublic{}, %DerivationPath{}) :: {:ok, %ExtendedPublic{}}
+  def from_derivation_path(%ExtendedPublic{} = public_key, %DerivationPath{} = derivation_path) do
+    ChildFromDerivationPath.get(public_key, derivation_path)
   end
 
   @doc """
