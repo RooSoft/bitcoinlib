13f460e1ef6931a0c7d555e991080a8cef932d4a
RooSoft <marc@roosoft.com>
2022-07-05 11:56:56 -0400

    added fingerprint append for private keys also

--- lib/key/hd/fingerprint.ex
+++ lib/key/hd/fingerprint.ex
@@ -45,6 +45,17 @@ defmodule BitcoinLib.Key.HD.Fingerprint do
   Adds a fingerprint to a public key
 
   ## Examples
+    iex> %BitcoinLib.Key.HD.ExtendedPrivate {
+    ...>   key: 0xE8F32E723DECF4051AEFAC8E2C93C9C5B214313817CDB01A1494B917C8436B35,
+    ...>   chain_code: 0x873DFF81C02F525623FD1FE5167EAC3A55A049DE3D314BB42EE227FFED37D508
+    ...> }
+    ...> |> BitcoinLib.Key.HD.Fingerprint.append()
+    %BitcoinLib.Key.HD.ExtendedPrivate {
+      fingerprint: 0x3442193E,
+      key: 0xE8F32E723DECF4051AEFAC8E2C93C9C5B214313817CDB01A1494B917C8436B35,
+      chain_code: 0x873DFF81C02F525623FD1FE5167EAC3A55A049DE3D314BB42EE227FFED37D508
+    }
+
     iex> %BitcoinLib.Key.HD.ExtendedPublic{
     ...>   key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
     ...>   chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
@@ -56,6 +67,16 @@ defmodule BitcoinLib.Key.HD.Fingerprint do
       chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
     }
   """
+  @spec append(%ExtendedPrivate{}) :: %ExtendedPrivate{}
+  def append(%ExtendedPrivate{} = private_key) do
+    fingerprint =
+      private_key
+      |> compute()
+
+    private_key
+    |> Map.put(:fingerprint, fingerprint)
+  end
+
   @spec append(%ExtendedPublic{}) :: %ExtendedPublic{}
   def append(%ExtendedPublic{} = public_key) do
     fingerprint =
--- test/key/hd/fingerprint_test.exs
+++ test/key/hd/fingerprint_test.exs
@@ -18,6 +18,23 @@ defmodule BitcoinLib.Key.HD.FingerprintTest do
     assert fingerprint == 0x18C1259
   end
 
+  test "append a private key fingerprint" do
+    private_key = %ExtendedPrivate{
+      key: 0xF79BB0D317B310B261A55A8AB393B4C8A1ABA6FA4D08AEF379CABA502D5D67F9,
+      chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
+    }
+
+    new_private_key =
+      private_key
+      |> Fingerprint.append()
+
+    assert %ExtendedPrivate{
+      fingerprint: 0x18C1259,
+      key: 0xF79BB0D317B310B261A55A8AB393B4C8A1ABA6FA4D08AEF379CABA502D5D67F9,
+      chain_code: 0x463223AAC10FB13F291A1BC76BC26003D98DA661CB76DF61E750C139826DEA8B
+    } == new_private_key
+  end
+
   test "compute a public key fingerprint" do
     public_key = %ExtendedPublic{
       key: 0x252C616D91A2488C1FD1F0F172E98F7D1F6E51F8F389B2F8D632A8B490D5F6DA9,
