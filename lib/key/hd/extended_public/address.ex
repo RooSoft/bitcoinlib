3ae7ebb38c5411225015ce0e64b613ff4061f8fd
RooSoft <marc@roosoft.com>
2022-07-06 12:44:19 -0400

    refactored BitcoinLib.Key.HD.ExtendedPublic.Address with telegraphic style

--- lib/key/hd/extended_public/address.ex
+++ lib/key/hd/extended_public/address.ex
@@ -3,18 +3,12 @@ defmodule BitcoinLib.Key.HD.ExtendedPublic.Address do
   alias BitcoinLib.Key.HD.ExtendedPublic.Address.{P2PKH, P2SH, Bech32}
 
   @spec from_extended_public_key(%ExtendedPublic{}, :p2pkh | :p2sh | :bech32) :: String.t()
-  def from_extended_public_key(%ExtendedPublic{} = public_key, :p2pkh) do
-    public_key
-    |> P2PKH.from_extended_public_key()
-  end
+  def from_extended_public_key(%ExtendedPublic{} = public_key, :p2pkh),
+    do: P2PKH.from_extended_public_key(public_key)
 
-  def from_extended_public_key(%ExtendedPublic{} = public_key, :p2sh) do
-    public_key
-    |> P2SH.from_extended_public_key()
-  end
+  def from_extended_public_key(%ExtendedPublic{} = public_key, :p2sh),
+    do: P2SH.from_extended_public_key(public_key)
 
-  def from_extended_public_key(%ExtendedPublic{} = public_key, :bech32) do
-    public_key
-    |> Bech32.from_extended_public_key()
-  end
+  def from_extended_public_key(%ExtendedPublic{} = public_key, :bech32),
+    do: Bech32.from_extended_public_key(public_key)
 end
