9feb92741b003324d397f633d7665a1431c4f231
RooSoft <marc@roosoft.com>
2022-07-06 08:31:27 -0400

    simplified p2sh code

--- lib/key/hd/extended_public/address/p2sh.ex
+++ lib/key/hd/extended_public/address/p2sh.ex
@@ -5,34 +5,42 @@ defmodule BitcoinLib.Key.HD.ExtendedPublic.Address.P2SH do
   def from_extended_public_key(%ExtendedPublic{key: key}) do
     key
     |> Binary.from_integer()
-    |> Crypto.hash160_bitstring()
+    |> hash160
     |> create_redeem_script
-    |> Crypto.hash160_bitstring()
+    |> hash160
     |> add_version_bytes
-    |> Binary.to_hex()
-    |> base58_encode()
+    |> append_checksum
+    |> base58_encode
+  end
+
+  defp hash160(value) do
+    value
+    |> Crypto.hash160_bitstring()
   end
 
   defp create_redeem_script(public_key_hash) do
     <<0x14::size(16), public_key_hash::bitstring>>
   end
 
-  defp add_version_bytes(hash) do
-    <<5::size(8), hash::bitstring>>
+  defp add_version_bytes(redeem_script) do
+    <<5::size(8), redeem_script::bitstring>>
   end
 
   defp base58_encode(value) do
     value
-    |> append_checksum
-    |> Binary.from_hex()
     |> Base58.encode()
   end
 
   defp append_checksum(public_key_hash) do
-    checksum =
+    hex_public_key_hash =
       public_key_hash
+      |> Binary.to_hex()
+
+    checksum =
+      hex_public_key_hash
       |> Crypto.checksum()
 
-    public_key_hash <> checksum
+    (hex_public_key_hash <> checksum)
+    |> Binary.from_hex()
   end
 end
