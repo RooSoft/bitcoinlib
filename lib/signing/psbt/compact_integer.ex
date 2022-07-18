09102205a488e962b197910e94e7972010e2b8fa
RooSoft <marc@roosoft.com>
2022-07-18 15:58:02 -0400

    added docs for CompactInteger

--- lib/signing/psbt/compact_integer.ex
+++ lib/signing/psbt/compact_integer.ex
@@ -1,5 +1,7 @@
 defmodule BitcoinLib.Signing.Psbt.CompactInteger do
   @moduledoc """
+  Extracts a variable length integer from a binary
+
   based on https://en.bitcoin.it/wiki/Protocol_documentation#Variable_length_integer
   """
 
@@ -7,26 +9,34 @@ defmodule BitcoinLib.Signing.Psbt.CompactInteger do
   @_32_bits_code 0xFE
   @_64_bits_code 0xFF
 
+  @doc """
+  Extracts a variable length integer from a binary according to the spec linked in the moduledoc,
+  returns a tuple containing the integer with the remaining of the binary
+  """
+  @spec extract_from(binary()) :: {integer(), binary()}
   def extract_from(<<@_16_bits_code::8, data::binary>>) do
     data
     |> extract_size(16)
   end
 
+  @spec extract_from(binary()) :: {integer(), binary()}
   def extract_from(<<@_32_bits_code::8, data::binary>>) do
     data
     |> extract_size(32)
   end
 
+  @spec extract_from(binary()) :: {integer(), binary()}
   def extract_from(<<@_64_bits_code::8, data::binary>>) do
     data
     |> extract_size(64)
   end
 
+  @spec extract_from(binary()) :: {integer(), binary()}
   def extract_from(<<value::8, remaining::binary>>) do
     {value, remaining}
   end
 
-  def extract_size(data, length) do
+  defp extract_size(data, length) do
     case data do
       <<value::size(length)>> -> {value, <<>>}
       <<value::size(length), remaining::binary>> -> {value, remaining}
