76e8f85e34d99713a411d090c751f02187e04775
RooSoft <marc@roosoft.com>
2022-07-19 09:22:12 -0400

    test extract a global module from a binary, with two keypairs and no remaining data

--- test/signing/psbt/global_test.exs
+++ test/signing/psbt/global_test.exs
@@ -3,7 +3,7 @@ defmodule BitcoinLib.Signing.Psbt.GlobalTest do
 
   doctest BitcoinLib.Signing.Psbt.Global
 
-  alias BitcoinLib.Signing.Psbt.{Global}
+  alias BitcoinLib.Signing.Psbt.{Global, Keypair}
   alias BitcoinLib.Signing.Psbt.Keypair.{Key, Value}
 
   @psbt_global_unsigned_tx 0
@@ -19,11 +19,25 @@ defmodule BitcoinLib.Signing.Psbt.GlobalTest do
       data
       |> Global.from_data()
 
-    keypair1 = global.keypairs |> List.first()
+    %Keypair{} = keypair1 = global.keypairs |> List.first()
 
     assert 1 == Enum.count(global.keypairs)
     assert %Key{keylen: 2, keytype: @psbt_global_unsigned_tx, keydata: <<1>>} == keypair1.key
     assert %Value{valuelen: 2, valuedata: <<1, 1>>} == keypair1.value
+    assert <<>> == remaining_data
+  end
+
+  test "extract a global module from a binary, with two keypairs and no remaining data" do
+    key = <<2, @psbt_global_unsigned_tx, 1>>
+    value = <<2, 1, 1>>
+    keypair = key <> value
+    data = <<keypair::binary, keypair::binary, @stop>>
+
+    {global, remaining_data} =
+      data
+      |> Global.from_data()
+
+    assert 2 == Enum.count(global.keypairs)
     assert <<>> == remaining_data
   end
 end
