5e5d77598b2a853ef403f46f0a4c4b026a53e8c1
RooSoft <marc@roosoft.com>
2022-07-19 08:30:58 -0400

    test parse a keypair, with remaining data

--- test/signing/psbt/keypair_test.exs
+++ test/signing/psbt/keypair_test.exs
@@ -13,12 +13,27 @@ defmodule BitcoinLib.Signing.Psbt.KeypairTest do
     value = <<2, 1, 1>>
     data = key <> value
 
-    { keypair, data } =
+    {keypair, data} =
       data
       |> Keypair.extract_from()
 
     assert %Key{keylen: 2, keytype: @psbt_global_unsigned_tx, keydata: <<1>>} == keypair.key
     assert %Value{valuelen: 2, valuedata: <<1, 1>>} == keypair.value
     assert <<>> == data
+  end
+
+  test "parse a keypair, with remaining data" do
+    key = <<3, @psbt_global_unsigned_tx, 9, 8>>
+    value = <<3, 5, 4, 6>>
+    remaining = <<4, 5, 6, 3>>
+    data = key <> value <> remaining
+
+    {keypair, remaining} =
+      data
+      |> Keypair.extract_from()
+
+    assert %Key{keylen: 3, keytype: @psbt_global_unsigned_tx, keydata: <<9, 8>>} == keypair.key
+    assert %Value{valuelen: 3, valuedata: <<5, 4, 6>>} == keypair.value
+    assert <<4, 5, 6, 3>> == remaining
   end
 end
