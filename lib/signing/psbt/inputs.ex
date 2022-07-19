5ad2067413ced11a320871d95cefb48951ef5206
RooSoft <marc@roosoft.com>
2022-07-19 14:08:51 -0400

    inputs now has utxo and witness? properties

--- lib/signing/psbt/inputs.ex
+++ lib/signing/psbt/inputs.ex
@@ -1,9 +1,10 @@
 defmodule BitcoinLib.Signing.Psbt.Inputs do
-  defstruct [:non_witness_utxo, unknowns: []]
+  defstruct [:utxo, :witness?, unknowns: []]
 
   alias BitcoinLib.Signing.Psbt.{Keypair, KeypairList, Inputs}
 
   @non_witness_utxo 0
+  @witness_utxo 1
 
   def from_keypair_list(%KeypairList{} = keypair_list) do
     keypair_list.keypairs
@@ -13,13 +14,21 @@ defmodule BitcoinLib.Signing.Psbt.Inputs do
   defp dispatch_keypair(%Keypair{key: key, value: value}, input) do
     case key.type do
       @non_witness_utxo -> add_non_witness_utxo(input, value)
+      @witness_utxo -> add_witness_utxo(input, value)
       _ -> add_unknown(input, key, value)
     end
   end
 
   defp add_non_witness_utxo(input, value) do
     input
-    |> Map.put(:non_witness_utxo, value.data)
+    |> Map.put(:utxo, value.data)
+    |> Map.put(:witness?, false)
+  end
+
+  defp add_witness_utxo(input, value) do
+    input
+    |> Map.put(:utxo, value.data)
+    |> Map.put(:witness?, true)
   end
 
   defp add_unknown(input, key, value) do
