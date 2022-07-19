0adce10f579439fb89a5f8d582e952213c783420
RooSoft <marc@roosoft.com>
2022-07-19 08:26:21 -0400

    coded and tested BitcoinLib.Signing.Psbt.Keypair

--- lib/signing/psbt/keypair.ex
+++ lib/signing/psbt/keypair.ex
@@ -1,27 +1,24 @@
-# defmodule BitcoinLib.Signing.Psbt.Keypair do
-#   @moduledoc """
-#   Extracts a keypair from a binary
-#   """
-#   defstruct [:key, :value]
+defmodule BitcoinLib.Signing.Psbt.Keypair do
+  @moduledoc """
+  Extracts a keypair from a binary according to the
+  [specification](https://github.com/bitcoin/bips/blob/master/bip-0174.mediawiki#specification)
 
-#   alias BitcoinLib.Signing.Psbt.Keypair
-#   alias BitcoinLib.Signing.Psbt.Keypair.{Key, Value}
+  <keypair> := <key> <value>
+  <key> := <keylen> <keytype> <keydata>
+  <value> := <valuelen> <valuedata>
+  """
+  defstruct [:key, :value]
 
-#   def extract_from(data) do
-#     {key, data} = extract_key(data)
-#     {value, data} = extract_value(data)
+  alias BitcoinLib.Signing.Psbt.Keypair
+  alias BitcoinLib.Signing.Psbt.Keypair.{Key, Value}
 
-#     %Keypair{key: key, value: value}
-#   end
+  def extract_from(data) do
+    {key, data} = extract_key(data)
+    {value, data} = extract_value(data)
 
-#   defp extract_key(data) do
-#     {keylen, data} = extract_keylen(data)
-#     {keytype, data} = extract_keytype(data)
-#     {keydata, data} = extract_keydata(data)
+    {%Keypair{key: key, value: value}, data}
+  end
 
-#   end
-
-#   defp extract_value(data) do
-
-#   end
-# end
+  defp extract_key(data), do: Key.extract_from(data)
+  defp extract_value(data), do: Value.extract_from(data)
+end
