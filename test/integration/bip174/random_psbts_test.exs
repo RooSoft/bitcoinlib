64d7906e956e53c2e8e6c32b9ef710007e429512
RooSoft <marc@roosoft.com>
2022-07-27 14:09:11 -0400

    test Case: PSBT with one P2SH-P2WSH input of a 2-of-2 multisig, redeemScript, witnessScript, and keypaths are available. Contains one signature.

--- test/integration/bip174/random_psbts_test.exs
+++ test/integration/bip174/random_psbts_test.exs
@@ -6,7 +6,16 @@ defmodule BitcoinLib.Test.Integration.Bip174.RandomPsbtsTest do
 
   alias BitcoinLib.Signing.Psbt
   alias BitcoinLib.Signing.Psbt.{Input, Output}
-  alias BitcoinLib.Signing.Psbt.Input.{NonWitnessUtxo, WitnessUtxo, FinalScriptSig, SighashType}
+
+  alias BitcoinLib.Signing.Psbt.Input.{
+    NonWitnessUtxo,
+    WitnessUtxo,
+    FinalScriptSig,
+    SighashType,
+    Bip32Derivation,
+    RedeemScript,
+    WitnessScript
+  }
 
   @doc """
   based on https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki#test-vectors
@@ -61,5 +70,23 @@ defmodule BitcoinLib.Test.Integration.Bip174.RandomPsbtsTest do
            ] = psbt.inputs
 
     assert [%Output{}, %Output{}] = psbt.outputs
+  end
+
+  test "Case: PSBT with one P2SH-P2WSH input of a 2-of-2 multisig, redeemScript, witnessScript, and keypaths are available. Contains one signature." do
+    base_64 =
+      "cHNidP8BAFUCAAAAASeaIyOl37UfxF8iD6WLD8E+HjNCeSqF1+Ns1jM7XLw5AAAAAAD/////AaBa6gsAAAAAGXapFP/pwAYQl8w7Y28ssEYPpPxCfStFiKwAAAAAAAEBIJVe6gsAAAAAF6kUY0UgD2jRieGtwN8cTRbqjxTA2+uHIgIDsTQcy6doO2r08SOM1ul+cWfVafrEfx5I1HVBhENVvUZGMEMCIAQktY7/qqaU4VWepck7v9SokGQiQFXN8HC2dxRpRC0HAh9cjrD+plFtYLisszrWTt5g6Hhb+zqpS5m9+GFR25qaAQEEIgAgdx/RitRZZm3Unz1WTj28QvTIR3TjYK2haBao7UiNVoEBBUdSIQOxNBzLp2g7avTxI4zW6X5xZ9Vp+sR/HkjUdUGEQ1W9RiED3lXR4drIBeP4pYwfv5uUwC89uq/hJ/78pJlfJvggg71SriIGA7E0HMunaDtq9PEjjNbpfnFn1Wn6xH8eSNR1QYRDVb1GELSmumcAAACAAAAAgAQAAIAiBgPeVdHh2sgF4/iljB+/m5TALz26r+En/vykmV8m+CCDvRC0prpnAAAAgAAAAIAFAACAAAA="
+
+    psbt = base_64 |> Psbt.parse()
+
+    assert [
+             %Input{
+               utxo: %WitnessUtxo{},
+               bip32_derivations: [%Bip32Derivation{}, %Bip32Derivation{}],
+               redeem_script: %RedeemScript{},
+               witness_script: %WitnessScript{}
+             }
+           ] = psbt.inputs
+
+    assert [] = psbt.outputs
   end
 end
