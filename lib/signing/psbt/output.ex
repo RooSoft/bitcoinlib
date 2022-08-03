9fe5f193b2bac73f7e425ca09b95377b2a9b3e6d
RooSoft <marc@roosoft.com>
2022-08-03 19:57:12 -0400

    refactored output to validate keys and dispatch keypairs

--- lib/signing/psbt/output.ex
+++ lib/signing/psbt/output.ex
@@ -9,6 +9,32 @@ defmodule BitcoinLib.Signing.Psbt.Output do
 
   def from_keypair_list(%KeypairList{} = keypair_list) do
     keypair_list.keypairs
+    |> validate_keys
+    |> dispatch_keypairs
+  end
+
+  defp validate_keys(keypairs) do
+    keys =
+      keypairs
+      |> Enum.map(& &1.key)
+
+    duplicate_keys = keys -- Enum.uniq(keys)
+
+    case duplicate_keys do
+      [] ->
+        {:ok, keypairs}
+
+      duplicates ->
+        {:error, "duplicate keys: #{inspect(duplicates)}"}
+    end
+  end
+
+  defp dispatch_keypairs({:error, message}) do
+    %{error: message}
+  end
+
+  defp dispatch_keypairs({:ok, keypairs}) do
+    keypairs
     |> Enum.reduce(%Output{}, &dispatch_keypair/2)
   end
 
