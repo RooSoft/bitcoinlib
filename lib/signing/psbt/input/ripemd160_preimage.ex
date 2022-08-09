9d8eab6a472a9e9d5d4cb22a172cd32e3277fe63
RooSoft <marc@roosoft.com>
2022-08-09 08:16:30 -0400

    validate input RIPEMD160 preimages

--- lib/signing/psbt/input/ripemd160_preimage.ex
+++ lib/signing/psbt/input/ripemd160_preimage.ex
@@ -2,15 +2,28 @@ defmodule BitcoinLib.Signing.Psbt.Input.Ripemd160Preimage do
   defstruct [:hash, :value]
 
   alias BitcoinLib.Signing.Psbt.Input.Ripemd160Preimage
+  alias BitcoinLib.Crypto
 
   def parse(<<preimage_hash::binary-size(20)>>, <<preimage::binary>>) do
-    %Ripemd160Preimage{
-      hash: preimage_hash,
-      value: preimage
-    }
+    case validate(preimage_hash, preimage) do
+      true ->
+        %Ripemd160Preimage{
+          hash: preimage_hash,
+          value: preimage
+        }
+
+      false ->
+        %{error: "RIPEMD160 preimage hash not matching hash in key"}
+    end
   end
 
   def parse(_) do
-    %{error: "invalid RIPEMD160 preimage"}
+    %{error: "invalid RIPEMD160 preimage format"}
+  end
+
+  defp validate(hash, preimage) do
+    hash ==
+      preimage
+      |> Crypto.ripemd160()
   end
 end
