d1f7276962ebeccfb9cdb3b049573eb7f8524c79
RooSoft <marc@roosoft.com>
2022-08-09 08:27:33 -0400

    validate input HASH256 preimages

--- lib/signing/psbt/input/hash256_preimage.ex
+++ lib/signing/psbt/input/hash256_preimage.ex
@@ -2,15 +2,28 @@ defmodule BitcoinLib.Signing.Psbt.Input.Hash256Preimage do
   defstruct [:hash, :value]
 
   alias BitcoinLib.Signing.Psbt.Input.Hash256Preimage
+  alias BitcoinLib.Crypto
 
-  def parse(<<preimage_hash::binary-size(20)>>, <<preimage::binary>>) do
-    %Hash256Preimage{
-      hash: preimage_hash,
-      value: preimage
-    }
+  def parse(<<preimage_hash::binary-size(32)>>, <<preimage::binary>>) do
+    case validate(preimage_hash, preimage) do
+      true ->
+        %Hash256Preimage{
+          hash: preimage_hash,
+          value: preimage
+        }
+
+      false ->
+        %{error: "HASH256 preimage hash not matching hash in key"}
+    end
   end
 
   def parse(_) do
     %{error: "invalid HASH256 preimage"}
+  end
+
+  defp validate(hash, preimage) do
+    hash ==
+      preimage
+      |> Crypto.double_sha256()
   end
 end
