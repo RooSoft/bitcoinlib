5d045d18b622ede8d378f60666001951b2011810
RooSoft <marc@roosoft.com>
2022-08-11 13:56:31 -0400

    added execute to HASH_160

--- lib/script/opcodes/crypto/hash160.ex
+++ lib/script/opcodes/crypto/hash160.ex
@@ -1,9 +1,21 @@
 defmodule BitcoinLib.Script.Opcodes.Crypto.Hash160 do
-  defstruct []
+  @behaviour BitcoinLib.Script.Opcode
+
+  defstruct type: BitcoinLib.Script.Opcodes.Crypto.Hash160
+
+  alias BitcoinLib.Crypto
 
   @value 0xA9
 
   def v do
     @value
+  end
+
+  def execute([]), do: {:error, "trying to execute OP_HASH160 on an empty stack"}
+
+  def execute([first_element | remaining]) do
+    hash = Crypto.hash160(first_element)
+
+    {:ok, [hash | remaining]}
   end
 end
