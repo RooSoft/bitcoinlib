72f6cf857983437a5ccfaac06cd7735c38ff3e11
RooSoft <marc@roosoft.com>
2022-08-11 14:00:14 -0400

    properly test HASH_160 opcode

---
+++ test/script/opcodes/crypto/hash160_test.exs
@@ -0,0 +1,31 @@
+defmodule BitcoinLib.Script.Opcodes.Crypto.Hash160Test do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Script.Opcodes.Crypto.Hash160
+
+  alias BitcoinLib.Script.Opcodes.Crypto.Hash160
+
+  @opcode_value 0xA9
+
+  test "OP_HASH160 opcode has the 0xA9 value" do
+    result = Hash160.v()
+
+    assert @opcode_value = result
+  end
+
+  test "execute OP_HASH160 on a stack" do
+    stack = ["6c7ab2f961a1bc3f13cdc08dc41c3f439adebd549a8ef1c089e81a5907376107"]
+
+    {:ok, result} = Hash160.execute(stack)
+
+    assert ["e96b467ff579a0b4bbb3941ef91442262af8fa39"] == result
+  end
+
+  test "execute OP_HASH160 on an empty stack" do
+    stack = []
+
+    {:error, message} = Hash160.execute(stack)
+
+    assert "trying to execute OP_HASH160 on an empty stack" == message
+  end
+end
