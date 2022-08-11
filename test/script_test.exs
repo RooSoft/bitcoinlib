e5fc9a598169cd0c723fc17b95e14ea81eeac834
RooSoft <marc@roosoft.com>
2022-08-11 13:47:30 -0400

    corrected the P2PKH script test

--- test/script_test.exs
+++ test/script_test.exs
@@ -22,9 +22,12 @@ defmodule BitcoinLib.ScriptTest do
   end
 
   test "parse a standard transaction to bitcoin address (pay-to-pubkey-hash)" do
+    pub_key_hash_size = 0x14
     pub_key_hash = 0x12AB8DC588CA9D5787DDE7EB29569DA63C3A238C
 
-    script = <<@dup::8, @hash_160::8, pub_key_hash::160, @equal_verify::8, @check_sig::8>>
+    script =
+      <<@dup::8, @hash_160::8, pub_key_hash_size::8, pub_key_hash::160, @equal_verify::8,
+        @check_sig::8>>
 
     sig =
       0x304502203F004EEED0CEF2715643E2F25A27A28F3C578E94C7F0F6A4DF104E7D163F7F8F022100B8B248C1CFD8F77A0365107A9511D759B7544D979DD152A955C867AFAC0EF78601
