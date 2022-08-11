41033615dc523964d76c0894e1b5cd6b364109c2
RooSoft <marc@roosoft.com>
2022-08-11 14:35:35 -0400

    added OP_EQUALVERIFY opcode with tests

--- lib/script/opcodes/bitwise_logic/equal_verify.ex
+++ lib/script/opcodes/bitwise_logic/equal_verify.ex
@@ -1,9 +1,24 @@
 defmodule BitcoinLib.Script.Opcodes.BitwiseLogic.EqualVerify do
   defstruct []
 
+  alias BitcoinLib.Script.Opcodes.BitwiseLogic.Equal
+  alias BitcoinLib.Script.Opcodes.FlowControl.Verify
+
   @value 0x88
 
   def v do
     @value
+  end
+
+  def execute([]), do: {:error, "trying to execute OP_EQUAL_VERIFY on an empty stack"}
+
+  def execute([element | []]),
+    do: {:error, "trying to execute OP_EQUAL_VERIFY on a stack with a single element #{element}"}
+
+  def execute([_first_element | [_second_element | _]] = stack) do
+    case Equal.execute(stack) do
+      {:ok, stack} -> Verify.execute(stack)
+      {:error, message} -> {:error, message}
+    end
   end
 end
---
+++ test/script/opcodes/bitwise_logic/equal_verify_test.exs
@@ -0,0 +1,39 @@
+defmodule BitcoinLib.Script.Opcodes.BitwiseLogic.EqualVerifyTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Script.Opcodes.BitwiseLogic.EqualVerify
+
+  alias BitcoinLib.Script.Opcodes.BitwiseLogic.EqualVerify
+
+  @opcode_value 0x88
+
+  test "OP_EQUAL_VERIFY opcode has the 0x88 value" do
+    result = EqualVerify.v()
+
+    assert @opcode_value = result
+  end
+
+  test "execute OP_EQUAL_VERIFY on an empty stack" do
+    stack = []
+
+    {:error, message} = EqualVerify.execute(stack)
+
+    assert "trying to execute OP_EQUAL_VERIFY on an empty stack" == message
+  end
+
+  test "execute OP_EQUAL_VERIFY on a stack with a single item" do
+    stack = [1]
+
+    {:error, message} = EqualVerify.execute(stack)
+
+    assert message =~ "trying to execute OP_EQUAL_VERIFY on a stack with a single element"
+  end
+
+  test "execute OP_EQUAL_VERIFY on a stack" do
+    stack = [5, 5, 6]
+
+    {:ok, stack} = EqualVerify.execute(stack)
+
+    assert [6] == stack
+  end
+end
