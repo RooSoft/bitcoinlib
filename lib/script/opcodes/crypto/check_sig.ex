648b5454d7c8da923cd1c57c4241b41e60aba9db
RooSoft <marc@roosoft.com>
2022-08-12 12:01:28 -0400

    added the Opcode behaviour to OP_CHECKSIG

--- lib/script/opcodes/crypto/check_sig.ex
+++ lib/script/opcodes/crypto/check_sig.ex
@@ -1,9 +1,15 @@
 defmodule BitcoinLib.Script.Opcodes.Crypto.CheckSig do
-  defstruct []
+  @behaviour BitcoinLib.Script.Opcode
+
+  defstruct type: BitcoinLib.Script.Opcodes.Crypto.CheckSig
 
   @value 0xAC
 
   def v do
     @value
+  end
+
+  def execute([_sig_pub_key | remaining]) do
+    {:ok, [1 | remaining]}
   end
 end
