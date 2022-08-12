5797510a899d1bca1d4c78bf43e911139fc9cf35
RooSoft <marc@roosoft.com>
2022-08-12 12:07:46 -0400

    added testnet capabilities for Address

--- lib/key/address.ex
+++ lib/key/address.ex
@@ -17,18 +17,18 @@ defmodule BitcoinLib.Key.Address do
     ...> |> BitcoinLib.Key.Address.from_public_key_hash(:p2pkh)
     "1Ak9NVPmwCHEpsSWvM6cNRC7dsYniRmwMG"
   """
-  @spec from_public_key_hash(integer(), :p2pkh | :p2sh) :: String.t()
-  def from_public_key_hash(public_key_hash, address_type \\ :p2sh) do
+  @spec from_public_key_hash(integer(), :p2pkh | :p2sh, :mainnet | :testnet) :: String.t()
+  def from_public_key_hash(public_key_hash, address_type \\ :p2sh, network \\ :mainnet) do
     public_key_hash
     |> Integer.to_string(16)
-    |> prepend_prefix(address_type)
+    |> prepend_prefix(address_type, network)
     |> append_checksum
     |> Binary.from_hex()
     |> Base58.encode()
   end
 
-  defp prepend_prefix(public_key_hash, address_type) do
-    get_prefix(address_type) <> public_key_hash
+  defp prepend_prefix(public_key_hash, address_type, network) do
+    get_prefix(address_type, network) <> public_key_hash
   end
 
   defp append_checksum(public_key_hash) do
@@ -39,10 +39,8 @@ defmodule BitcoinLib.Key.Address do
     public_key_hash <> checksum
   end
 
-  defp get_prefix(address_type) do
-    case address_type do
-      :p2pkh -> "00"
-      :p2sh -> "05"
-    end
-  end
+  defp get_prefix(:p2pkh, :mainnet), do: "00"
+  defp get_prefix(:p2sh, :mainnet), do: "05"
+  defp get_prefix(:p2pkh, :testnet), do: "6F"
+  defp get_prefix(:p2sh, :testnet), do: "C4"
 end
