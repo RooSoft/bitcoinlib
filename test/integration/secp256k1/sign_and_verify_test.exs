d108e909ca0ada2f9c5beb5c462f7fd95998ea06
RooSoft <marc@roosoft.com>
2022-08-16 09:35:07 -0400

    added some integration tests for secp256k1

--- test/integration/secp256k1/sign_and_verify_test.exs
+++ test/integration/secp256k1/sign_and_verify_test.exs
@@ -9,11 +9,32 @@ defmodule BitcoinLib.Test.Integration.Secp256k1.SignAndVerivy do
         <<0xD6EAD233E06C068585976B5C8373861D77E7F030EC452E65EE81C85FA6906970::256>>
       )
 
-    message = "this is the message to be signed"
+    message = "76a914725ebac06343111227573d0b5287954ef9b88aae88ac"
 
-    signature = :crypto.sign(:ecdsa, :sha256, message, [prv, :secp256k1])
+    signature = BitcoinLib.Crypto.Secp256k1.sign(message, prv)
 
-    assert :crypto.verify(:ecdsa, :sha256, message, signature, [pub, :secp256k1])
+    assert BitcoinLib.Crypto.Secp256k1.validate(signature, message, pub)
+  end
+
+  test "verify a signature with a compressed public key" do
+    {pub, prv} =
+      :crypto.generate_key(
+        :ecdh,
+        :secp256k1,
+        <<0xD6EAD233E06C068585976B5C8373861D77E7F030EC452E65EE81C85FA6906970::256>>
+      )
+
+    {uncompressed_pub, compressed_pub} =
+      <<0xD6EAD233E06C068585976B5C8373861D77E7F030EC452E65EE81C85FA6906970::256>>
+      |> BitcoinLib.Key.Public.from_private_key()
+
+    assert uncompressed_pub == pub
+
+    message = "76a914725ebac06343111227573d0b5287954ef9b88aae88ac"
+
+    signature = BitcoinLib.Crypto.Secp256k1.sign(message, prv)
+
+    assert BitcoinLib.Crypto.Secp256k1.validate(signature, message, compressed_pub)
   end
 
   test "compare public keys from :crypto and BitcoinLib" do
@@ -26,9 +47,24 @@ defmodule BitcoinLib.Test.Integration.Secp256k1.SignAndVerivy do
         private_key_in
       )
 
-    {long_bitcoinlib_pub_key, _short_version} = BitcoinLib.Key.Public.from_private_key(private_key_out)
+    {long_bitcoinlib_pub_key, _short_version} =
+      BitcoinLib.Key.Public.from_private_key(private_key_out)
 
     assert public_key_out == long_bitcoinlib_pub_key
     assert private_key_in == private_key_out
+  end
+
+  test "secp256k1 issues ever changing signatures" do
+    prv = <<0xD6EAD233E06C068585976B5C8373861D77E7F030EC452E65EE81C85FA6906970::256>>
+    message = "76a914725ebac06343111227573d0b5287954ef9b88aae88ac"
+
+    sig1 = :crypto.sign(:ecdsa, :sha256, message, [prv, :secp256k1])
+    sig2 = :crypto.sign(:ecdsa, :sha256, message, [prv, :secp256k1])
+    sig3 = :crypto.sign(:ecdsa, :sha256, message, [prv, :secp256k1])
+    sig4 = :crypto.sign(:ecdsa, :sha256, message, [prv, :secp256k1])
+
+    assert sig1 != sig2
+    assert sig2 != sig3
+    assert sig3 != sig4
   end
 end
