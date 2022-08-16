4c425a4a4a0431fa021fba31b197cf5c61c46a80
RooSoft <marc@roosoft.com>
2022-08-16 15:21:32 -0400

    added script formats as comments above the identify overloads

--- lib/script/analyzer.ex
+++ lib/script/analyzer.ex
@@ -10,15 +10,18 @@ defmodule BitcoinLib.Script.Analyzer do
   @hash160 Crypto.Hash160.v()
   @check_sig Crypto.CheckSig.v()
 
+  # 41 <<_pub_key::520>> ac
   def identify(<<@uncompressed_pub_key_size::8, _pub_key::bitstring-520, @check_sig::8>>),
     do: :p2pk
 
+  # 76 a9 14 <<_pub_key_hash::160>> 88 ac
   def identify(
         <<@dup::8, @hash160::8, @pub_key_hash_size::8, _pub_key_hash::bitstring-160,
           @equal_verify::8, @check_sig::8>>
       ),
       do: :p2pkh
 
+  # a9 14 <<_script_hash::160>> 87
   def identify(<<@hash160, @pub_key_hash_size, _script_hash::bitstring-160, @equal>>), do: :p2sh
 
   def identify(script) when is_bitstring(script), do: :unknown
