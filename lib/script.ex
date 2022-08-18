5e2b92dd2857a16473d65812ae8af54aa4167bfe
RooSoft <marc@roosoft.com>
2022-08-18 09:35:16 -0400

    created the script runner, simplifying BitcoinLib.Script

--- lib/script.ex
+++ lib/script.ex
@@ -1,49 +1,21 @@
 defmodule BitcoinLib.Script do
-  alias BitcoinLib.Script.{Analyzer, OpcodeManager, Parser}
+  alias BitcoinLib.Script.{Analyzer, Parser, Runner}
 
   @spec parse(bitstring()) :: list()
   def parse(script) when is_bitstring(script) do
-    Parser.parse(script)
+    script
+    |> Parser.parse()
   end
 
   @spec execute(bitstring(), list()) :: {:ok, boolean()} | {:error, binary()}
   def execute(script, stack) when is_bitstring(script) do
-    {:ok, stack, script, script}
-    |> execute_next_opcode
+    script
+    |> Runner.execute(stack)
   end
 
   @spec identify(bitstring()) :: :unknown | :p2pk | :p2pkh | :p2sh
   def identify(script) when is_bitstring(script) do
     script
     |> Analyzer.identify()
-  end
-
-  defp execute_next_opcode({:error, message}), do: {:error, message}
-
-  defp execute_next_opcode({:empty_script, [1]}), do: {:ok, true}
-  defp execute_next_opcode({:empty_script, _}), do: {:ok, false}
-
-  defp execute_next_opcode({:ok, stack, script, whole_script}) do
-    script
-    |> OpcodeManager.extract_from_script(whole_script)
-    |> execute_opcode(stack, whole_script)
-    |> execute_next_opcode
-  end
-
-  defp execute_opcode({:empty_script}, stack, _whole_script), do: {:empty_script, stack}
-
-  defp execute_opcode({:error, message}, _stack, _whole_script) do
-    {:error, message}
-  end
-
-  defp execute_opcode({:opcode, opcode, remaining}, stack, whole_script) do
-    case opcode.__struct__.execute(opcode, stack) do
-      {:ok, stack} -> {:ok, stack, remaining, whole_script}
-      {:error, message} -> {:error, message}
-    end
-  end
-
-  defp execute_opcode({:data, data, remaining}, stack, whole_script) do
-    {:ok, [data | stack], remaining, whole_script}
   end
 end
---
+++ lib/script/runner.ex
@@ -0,0 +1,37 @@
+defmodule BitcoinLib.Script.Runner do
+  alias BitcoinLib.Script.{OpcodeManager}
+
+  def execute(script, stack) when is_bitstring(script) do
+    {:ok, stack, script, script}
+    |> execute_next_opcode
+  end
+
+  defp execute_next_opcode({:error, message}), do: {:error, message}
+
+  defp execute_next_opcode({:empty_script, [1]}), do: {:ok, true}
+  defp execute_next_opcode({:empty_script, _}), do: {:ok, false}
+
+  defp execute_next_opcode({:ok, stack, script, whole_script}) do
+    script
+    |> OpcodeManager.extract_from_script(whole_script)
+    |> execute_opcode(stack, whole_script)
+    |> execute_next_opcode
+  end
+
+  defp execute_opcode({:empty_script}, stack, _whole_script), do: {:empty_script, stack}
+
+  defp execute_opcode({:error, message}, _stack, _whole_script) do
+    {:error, message}
+  end
+
+  defp execute_opcode({:opcode, opcode, remaining}, stack, whole_script) do
+    case opcode.__struct__.execute(opcode, stack) do
+      {:ok, stack} -> {:ok, stack, remaining, whole_script}
+      {:error, message} -> {:error, message}
+    end
+  end
+
+  defp execute_opcode({:data, data, remaining}, stack, whole_script) do
+    {:ok, [data | stack], remaining, whole_script}
+  end
+end
