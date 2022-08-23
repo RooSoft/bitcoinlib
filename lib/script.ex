d9bcd766d07b714d1b78679ce0ba450d1fa61302
marc@roosoft.com <marc@roosoft.com>
2022-08-23 10:37:07 -0400

    added some doctest for Script.parse

--- lib/script.ex
+++ lib/script.ex
@@ -1,6 +1,24 @@
 defmodule BitcoinLib.Script do
   alias BitcoinLib.Script.{Analyzer, Parser, Runner}
 
+  @doc """
+  Transforms a script in the bitstring form into a list of opcodes
+
+  ## Examples
+    iex> <<0x76a914fde0a08625e327ba400644ad62d5c571d2eec3de88ac::200>>
+    ...> |> BitcoinLib.Script.parse()
+    [
+      %BitcoinLib.Script.Opcodes.Stack.Dup{},
+      %BitcoinLib.Script.Opcodes.Crypto.Hash160{},
+      %BitcoinLib.Script.Opcodes.Data{
+        value: <<0xfde0a08625e327ba400644ad62d5c571d2eec3de::160>>
+      },
+      %BitcoinLib.Script.Opcodes.BitwiseLogic.EqualVerify{},
+      %BitcoinLib.Script.Opcodes.Crypto.CheckSig{
+        script: <<0x76a914fde0a08625e327ba400644ad62d5c571d2eec3de88ac::200>>
+      }
+    ]
+  """
   @spec parse(bitstring()) :: list()
   def parse(script) when is_bitstring(script) do
     script
