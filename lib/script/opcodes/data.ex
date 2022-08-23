d1ac02592832052771d80ffc7b3458b24285edb7
marc@roosoft.com <marc@roosoft.com>
2022-08-23 11:31:12 -0400

    pretty print data structures when sent to IO.inspect

--- lib/script/opcodes/data.ex
+++ lib/script/opcodes/data.ex
@@ -1,3 +1,14 @@
 defmodule BitcoinLib.Script.Opcodes.Data do
   defstruct [:value]
 end
+
+defimpl Inspect, for: BitcoinLib.Script.Opcodes.Data do
+  @byte 8
+
+  def inspect(data, _opts) do
+    hex_data = Base.encode16(data.value, case: :lower)
+    data_size = byte_size(data.value) * @byte
+
+    "%BitcoinLib.Script.Opcodes.Data{value: <<0x#{hex_data}::#{data_size}>>}"
+  end
+end
