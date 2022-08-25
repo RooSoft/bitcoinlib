7f245d5b6fc442ba005006b1c17455e0ad6b61de
marc@roosoft.com <marc@roosoft.com>
2022-08-25 13:31:34 -0400

    test display a hex binary

--- test/formatting/hex_binary_test.exs
+++ test/formatting/hex_binary_test.exs
@@ -12,4 +12,12 @@ defmodule BitcoinLib.Formatting.HexBinaryTest do
 
     assert %HexBinary{data: <<0x12345678::32>>} = hex_binary
   end
+
+  test "display a hex binary" do
+    hex_binary = HexBinary.from_binary(<<0x12345678::32>>)
+
+    formatted = "#{inspect hex_binary}"
+
+    assert "<<0x12345678::32>>" == formatted
+  end
 end
