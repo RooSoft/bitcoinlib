1c2c7990eb9828bf9da7a253174df217397826c9
marc@roosoft.com <marc@roosoft.com>
2022-08-26 12:31:25 -0400

    put outputs in the right order in a transaction

--- lib/transaction.ex
+++ lib/transaction.ex
@@ -86,7 +86,7 @@ defmodule BitcoinLib.Transaction do
       end)
 
     %{map | remaining: remaining}
-    |> Map.put(:outputs, outputs |> Enum.reverse())
+    |> Map.put(:outputs, Enum.reverse(outputs))
   end
 
   defp extract_locktime(%{remaining: remaining} = map) do
