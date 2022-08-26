90be80b987ef0db7acc9da327dc74a0ff4a03121
marc@roosoft.com <marc@roosoft.com>
2022-08-26 12:35:14 -0400

    commented unused code thats supposed to be called by commented code

--- lib/signing/psbt/input/non_witness_utxo.ex
+++ lib/signing/psbt/input/non_witness_utxo.ex
@@ -4,10 +4,10 @@ defmodule BitcoinLib.Signing.Psbt.Input.NonWitnessUtxo do
   alias BitcoinLib.Signing.Psbt.Input.NonWitnessUtxo
   alias BitcoinLib.Signing.Psbt.Keypair
   alias BitcoinLib.Signing.Psbt.Keypair.{Key, Value}
-  alias BitcoinLib.Script
+ # alias BitcoinLib.Script
 
   alias BitcoinLib.Transaction
-  alias BitcoinLib.Transaction.Output
+ # alias BitcoinLib.Transaction.Output
 
   def parse(keypair) do
     %{keypair: keypair, non_witness_utxo: %NonWitnessUtxo{}}
@@ -41,42 +41,42 @@ defmodule BitcoinLib.Signing.Psbt.Input.NonWitnessUtxo do
     end
   end
 
-  defp validate_script_pub_keys(%{non_witness_utxo: %{error: _message}} = map),
-    do: map
+  # defp validate_script_pub_keys(%{non_witness_utxo: %{error: _message}} = map),
+  #   do: map
 
-  defp validate_script_pub_keys(
-         %{
-           non_witness_utxo: %NonWitnessUtxo{transaction: %Transaction{outputs: outputs}}
-         } = map
-       ) do
-    non_witness_type =
-      outputs
-      |> Enum.map(&get_script_type/1)
-      |> Enum.filter(&(&1 != :non_witness))
-      |> List.first()
+  # defp validate_script_pub_keys(
+  #        %{
+  #          non_witness_utxo: %NonWitnessUtxo{transaction: %Transaction{outputs: outputs}}
+  #        } = map
+  #      ) do
+  #   non_witness_type =
+  #     outputs
+  #     |> Enum.map(&get_script_type/1)
+  #     |> Enum.filter(&(&1 != :non_witness))
+  #     |> List.first()
 
-    case non_witness_type do
-      nil ->
-        map
+  #   case non_witness_type do
+  #     nil ->
+  #       map
 
-      :witness ->
-        set_error(map, "a non-witness UTXO contains a witness script pub key")
+  #     :witness ->
+  #       set_error(map, "a non-witness UTXO contains a witness script pub key")
 
-      :unknown ->
-        set_error(map, "a non-witness UTXO contains an unknown script pub key")
-    end
-  end
+  #     :unknown ->
+  #       set_error(map, "a non-witness UTXO contains an unknown script pub key")
+  #   end
+  # end
 
-  defp get_script_type(%Output{script_pub_key: script_pub_key}) do
-    case Script.identify(script_pub_key) do
-      :p2pk -> :non_witness
-      :p2pkh -> :non_witness
-      :p2sh -> :non_witness
-      :p2wpkh -> :witness
-      :p2wsh -> :witness
-      _ -> :unknown
-    end
-  end
+  # defp get_script_type(%Output{script_pub_key: script_pub_key}) do
+  #   case Script.identify(script_pub_key) do
+  #     :p2pk -> :non_witness
+  #     :p2pkh -> :non_witness
+  #     :p2sh -> :non_witness
+  #     :p2wpkh -> :witness
+  #     :p2wsh -> :witness
+  #     _ -> :unknown
+  #   end
+  # end
 
   defp set_transaction(%{non_witness_utxo: non_witness_utxo} = map, transaction) do
     %{map | non_witness_utxo: Map.put(non_witness_utxo, :transaction, transaction)}
