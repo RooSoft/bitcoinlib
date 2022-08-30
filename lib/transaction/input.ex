8e69d1c52ae46fc5ec28465f3c36198d879ec54d
marc@roosoft.com <marc@roosoft.com>
2022-08-30 12:56:41 -0400

    transaction inputs can now encode themselves

--- lib/transaction/input.ex
+++ lib/transaction/input.ex
@@ -5,11 +5,12 @@ defmodule BitcoinLib.Transaction.Input do
   Based on https://learnmeabitcoin.com/technical/input
   """
 
-  @bits 8
+  @byte 8
 
   alias BitcoinLib.Signing.Psbt.CompactInteger
   alias BitcoinLib.Transaction.Input
   alias BitcoinLib.Script
+  alias BitcoinLib.Signing.Psbt.CompactInteger
 
   def extract_from(<<txid::little-256, vout::little-32, remaining::bitstring>>) do
     {script_sig, remaining} = extract_script_sig(remaining)
@@ -26,11 +27,47 @@ defmodule BitcoinLib.Transaction.Input do
     }
   end
 
+  @doc """
+  Encodes an input into a bitstring
+
+  ## Examples
+
+    iex> %BitcoinLib.Transaction.Input{
+    ...>   sequence: 0xFFFFFFFF,
+    ...>   txid: "5e2383defe7efcbdc9fdd6dba55da148b206617bbb49e6bb93fce7bfbb459d44",
+    ...>   vout: 1
+    ...> } |> BitcoinLib.Transaction.Input.encode()
+    <<0x449d45bbbfe7fc93bbe649bb7b6106b248a15da5dbd6fdc9bdfc7efede83235e0100000000ffffffff::328>>
+  """
+  def encode(%Input{} = input) do
+    little_endian_txid =
+      Binary.from_hex(input.txid)
+      |> :binary.decode_unsigned(:big)
+      |> :binary.encode_unsigned(:little)
+
+    {script_size, script} = format_script_sig(input.script_sig)
+
+    <<script_size::bitstring, script::bitstring>>
+
+    <<little_endian_txid::bitstring-256, input.vout::little-32, script_size::bitstring,
+      script::bitstring, input.sequence::little-32>>
+  end
+
+  defp format_script_sig(nil), do: {<<0::8>>, <<>>}
+
+  defp format_script_sig(script_sig) do
+    script_size =
+      (byte_size(script_sig) * @byte)
+      |> CompactInteger.encode()
+
+    {script_size, script_sig}
+  end
+
   defp extract_script_sig(remaining) do
     %CompactInteger{value: script_sig_size, remaining: remaining} =
       CompactInteger.extract_from(remaining)
 
-    script_sig_bit_size = script_sig_size * @bits
+    script_sig_bit_size = script_sig_size * @byte
 
     <<script_sig::bitstring-size(script_sig_bit_size), remaining::bitstring>> = remaining
 
---
+++ test/transaction/input_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinLib.Transaction.InputTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Transaction.Input
+end
