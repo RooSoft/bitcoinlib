172d28a52d4e4b1832ddb67c106402d92b1a0e4e
marc@roosoft.com <marc@roosoft.com>
2022-08-30 11:57:38 -0400

    add encoding capacites to CompactInteger

--- lib/signing/psbt/compact_integer.ex
+++ lib/signing/psbt/compact_integer.ex
@@ -9,9 +9,49 @@ defmodule BitcoinLib.Signing.Psbt.CompactInteger do
 
   alias BitcoinLib.Signing.Psbt.CompactInteger
 
+  @_8_bits_code 0xFC
   @_16_bits_code 0xFD
   @_32_bits_code 0xFE
   @_64_bits_code 0xFF
+
+  @max_8_bits_value @_8_bits_code
+  @max_16_bits_value 0xFFFF
+  @max_32_bits_value 0xFFFFFFFF
+  @max_64_bits_value 0xFFFFFFFFFFFFFFFF
+
+  @doc """
+  Encodes an integer according to the spec linked to in the moduledoc
+  """
+  @spec encode(integer(), :big_endian | :little_endian) :: bitstring()
+  def encode(value, endianness \\ :little_endian)
+
+  def encode(value, _) when value <= @max_8_bits_value do
+    <<value::8>>
+  end
+
+  def encode(value, :little_endian) when value <= @max_16_bits_value do
+    <<@_16_bits_code::8, value::little-16>>
+  end
+
+  def encode(value, :little_endian) when value <= @max_32_bits_value do
+    <<@_32_bits_code::8, value::little-32>>
+  end
+
+  def encode(value, :little_endian) when value <= @max_64_bits_value do
+    <<@_64_bits_code::8, value::little-64>>
+  end
+
+  def encode(value, :big_endian) when value <= @max_16_bits_value do
+    <<0xFD::8, value::16>>
+  end
+
+  def encode(value, :big_endian) when value <= @max_32_bits_value do
+    <<0xFE::8, value::32>>
+  end
+
+  def encode(value, :big_endian) when value <= @max_64_bits_value do
+    <<0xFF::8, value::64>>
+  end
 
   @doc """
   Extracts a variable length integer from a bitstring according to the spec linked in the moduledoc,
--- test/signing/psbt/compact_integer_test.exs
+++ test/signing/psbt/compact_integer_test.exs
@@ -5,6 +5,48 @@ defmodule BitcoinLib.Signing.Psbt.CompactIntegerTest do
 
   alias BitcoinLib.Signing.Psbt.CompactInteger
 
+  test "encode a 8 bits integer in the compact form" do
+    encoded = CompactInteger.encode(0xAB)
+
+    assert <<0xAB>> = encoded
+  end
+
+  test "encode a 16 bits integer in the compact little endian form" do
+    encoded = CompactInteger.encode(0xFED)
+
+    assert <<0xFDED0F::24>> = encoded
+  end
+
+  test "encode a 32 bits integer in the compact little endian form" do
+    encoded = CompactInteger.encode(0xFEDCB)
+
+    assert <<0xFECBED0F00::40>> = encoded
+  end
+
+  test "encode a 64 bits integer in the compact little endian form" do
+    encoded = CompactInteger.encode(0xFEDCBA987)
+
+    assert <<0xFF87A9CBED0F000000::72>> = encoded
+  end
+
+  test "encode a 16 bits integer in the compact big endian form" do
+    encoded = CompactInteger.encode(0xFED, :big_endian)
+
+    assert <<0xFD0FED::24>> = encoded
+  end
+
+  test "encode a 32 bits integer in the compact big endian form" do
+    encoded = CompactInteger.encode(0xFEDCB, :big_endian)
+
+    assert <<0xFE000FEDCB::40>> = encoded
+  end
+
+  test "encode a 64 bits integer in the compact big endian form" do
+    encoded = CompactInteger.encode(0xFEDCBA987, :big_endian)
+
+    assert <<0xFF0000000FEDCBA987::72>> = encoded
+  end
+
   test "parse a 8 bits compact integer, with no rest" do
     data = <<2>>
 
@@ -38,7 +80,7 @@ defmodule BitcoinLib.Signing.Psbt.CompactIntegerTest do
   test "parse a 64 bits compact integer, with no rest" do
     data = <<0xFF, 1, 2, 3, 4, 5, 6, 7, 8>>
 
-    compact_integer=
+    compact_integer =
       data
       |> CompactInteger.extract_from()
 
@@ -54,7 +96,6 @@ defmodule BitcoinLib.Signing.Psbt.CompactIntegerTest do
 
     assert %CompactInteger{value: 2, size: 8, remaining: <<3, 4>>} == compact_integer
   end
-
 
   test "parse a 16 bits compact integer, with something remaining" do
     data = <<0xFD, 1, 2, 1, 2, 3>>
@@ -83,6 +124,7 @@ defmodule BitcoinLib.Signing.Psbt.CompactIntegerTest do
       data
       |> CompactInteger.extract_from()
 
-    assert %CompactInteger{value: 0x807060504030201, size: 64, remaining: <<2, 1>>} == compact_integer
+    assert %CompactInteger{value: 0x807060504030201, size: 64, remaining: <<2, 1>>} ==
+             compact_integer
   end
 end
