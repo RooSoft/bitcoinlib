41d701851755741f7d3d023c4a6ebf0899e417d6
marc@roosoft.com <marc@roosoft.com>
2022-09-04 18:48:13 -0400

    removed an unused statement from a test

--- test/integration/locking_scripts/manual_example_test.exs
+++ test/integration/locking_scripts/manual_example_test.exs
@@ -27,9 +27,6 @@ defmodule BitcoinLib.Test.Integration.LockingScripts.ManualExampleTest do
       public_key
       |> PublicKeyHash.from_public_key()
 
-    public_key_hash
-    |> BitcoinLib.Key.Address.from_public_key_hash(:p2pkh, :testnet)
-
     # the transaction can be found in a block explorer such as here:
     # https://mempool.space/testnet/tx/e4c226432a9319d603b2ed1fa609bffe4cd91f89b3176a9e73b19f7891a92bb6
     # we use the first UTXO's script pub key from the output in the above transaction:
@@ -98,7 +95,8 @@ defmodule BitcoinLib.Test.Integration.LockingScripts.ManualExampleTest do
       Transaction.encode(transaction)
       |> Binary.to_hex()
 
-    assert "0100000001b62ba991789fb1739e6a17b3891fd94cfebf09a61fedb203d619932a4326c2e4000000006a4730440220032a1544f599bf29981851e826e8a6f7c036958ba3543cf9778a0756dfc425f6022067eec131c0d73825633c0fddce1abfb14bb26bc9e0d6e9d644a77361f74cb55c012103f0e5a53db9f85e5b2eecf677925ffe21dd1409bcfe9a0730404053599b0901e5ffffffff0110270000000000001976a914afc3e518577316386188af748a816cd14ce333f288ac00000000" = encoded_transaction
+    assert "0100000001b62ba991789fb1739e6a17b3891fd94cfebf09a61fedb203d619932a4326c2e4000000006a4730440220032a1544f599bf29981851e826e8a6f7c036958ba3543cf9778a0756dfc425f6022067eec131c0d73825633c0fddce1abfb14bb26bc9e0d6e9d644a77361f74cb55c012103f0e5a53db9f85e5b2eecf677925ffe21dd1409bcfe9a0730404053599b0901e5ffffffff0110270000000000001976a914afc3e518577316386188af748a816cd14ce333f288ac00000000" =
+             encoded_transaction
   end
 
   defp append_sighash(transaction, sighash) do
