ea68dea8a37ded6d7e9f784e0f911c67e81dc6d5
marc@roosoft.com <marc@roosoft.com>
2022-09-04 19:08:30 -0400

    added test: same bitcoin testnet signing own P2PKH, but using Transaction.sign_and_encode

--- test/integration/locking_scripts/manual_example_test.exs
+++ test/integration/locking_scripts/manual_example_test.exs
@@ -99,6 +99,55 @@ defmodule BitcoinLib.Test.Integration.LockingScripts.ManualExampleTest do
              encoded_transaction
   end
 
+  test "same bitcoin testnet signing own P2PKH, but using Transaction.sign_and_encode" do
+    private_key =
+      "rally celery split order almost twenty ignore record legend learn chaos decade"
+      |> PrivateKey.from_mnemonic_phrase()
+      |> PrivateKey.from_derivation_path("m/44'/1'/0'/0/0")
+      |> elem(1)
+
+    public_key_hash =
+      private_key
+      |> PublicKey.from_private_key()
+      |> PublicKeyHash.from_public_key()
+
+    # the transaction can be found in a block explorer such as here:
+    # https://mempool.space/testnet/tx/e4c226432a9319d603b2ed1fa609bffe4cd91f89b3176a9e73b19f7891a92bb6
+    # we use the first UTXO's script pub key from the output in the above transaction:
+    redeem_script =
+      <<0x76A914AFC3E518577316386188AF748A816CD14CE333F288AC::200>>
+      |> Script.parse()
+
+    # found straight in the output of step1 in here:
+    # https://medium.com/@bitaps.com/exploring-bitcoin-signing-the-p2pkh-input-b8b4d5c4809c#50a6
+    locking_script = [
+      %Opcodes.Stack.Dup{},
+      %Opcodes.Crypto.Hash160{},
+      %Opcodes.Data{value: public_key_hash},
+      %Opcodes.BitwiseLogic.EqualVerify{},
+      %Opcodes.Crypto.CheckSig{}
+    ]
+
+    signed_transaction =
+      %Transaction{
+        version: 1,
+        inputs: [
+          %Input{
+            txid: "e4c226432a9319d603b2ed1fa609bffe4cd91f89b3176a9e73b19f7891a92bb6",
+            vout: 0,
+            sequence: 0xFFFFFFFF,
+            script_sig: redeem_script
+          }
+        ],
+        outputs: [%Output{script_pub_key: locking_script, value: 10_000}],
+        locktime: 0
+      }
+      |> Transaction.sign_and_encode(private_key)
+
+    assert "0100000001b62ba991789fb1739e6a17b3891fd94cfebf09a61fedb203d619932a4326c2e4000000006a4730440220032a1544f599bf29981851e826e8a6f7c036958ba3543cf9778a0756dfc425f6022067eec131c0d73825633c0fddce1abfb14bb26bc9e0d6e9d644a77361f74cb55c012103f0e5a53db9f85e5b2eecf677925ffe21dd1409bcfe9a0730404053599b0901e5ffffffff0110270000000000001976a914afc3e518577316386188af748a816cd14ce333f288ac00000000" ==
+             signed_transaction
+  end
+
   defp append_sighash(transaction, sighash) do
     <<transaction::bitstring, sighash::little-32>>
   end
