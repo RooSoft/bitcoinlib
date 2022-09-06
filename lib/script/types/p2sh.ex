6711b0e4fbe7431d61321d35dde14410b34518ec
marc@roosoft.com <marc@roosoft.com>
2022-09-06 19:15:19 -0400

    added tests for P2SH

--- lib/script/types/p2sh.ex
+++ lib/script/types/p2sh.ex
@@ -6,6 +6,15 @@ defmodule BitcoinLib.Script.Types.P2sh do
 
   @doc """
   Returns a full P2SH script out of a public key hash
+
+  ## Examples
+    iex> <<0x11c371a2b2d22c7b8b1b51d9fde0e44a9dfdc7bb::160>>
+    ...> |> BitcoinLib.Script.Types.P2sh.create()
+    [
+      %BitcoinLib.Script.Opcodes.Crypto.Hash160{},
+      %BitcoinLib.Script.Opcodes.Data{value: <<0x11c371a2b2d22c7b8b1b51d9fde0e44a9dfdc7bb::160>>},
+      %BitcoinLib.Script.Opcodes.BitwiseLogic.Equal{}
+    ]
   """
   def create(script_hash) do
     [
