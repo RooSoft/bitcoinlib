508485073b2e0619e6feb58dbed7eceaaa3c0c35
marc@roosoft.com <marc@roosoft.com>
2022-09-07 11:59:18 -0400

    added typespecs and default values to Address.to_public_key_hash

--- lib/key/address.ex
+++ lib/key/address.ex
@@ -35,6 +35,7 @@ defmodule BitcoinLib.Key.Address do
     ...> BitcoinLib.Key.Address.to_public_key_hash(address)
     {:ok, <<0xafc3e518577316386188af748a816cd14ce333f2::160>>, :p2pkh}
   """
+  @spec to_public_key_hash(binary()) :: {:ok, <<_::160>>, atom()} | {:error, binary()}
   def to_public_key_hash(address) do
     <<prefix::8, public_key_hash::bitstring-160, checksum::bitstring-32>> =
       address
@@ -76,9 +77,11 @@ defmodule BitcoinLib.Key.Address do
   defp get_prefix(:p2sh, :mainnet), do: "05"
   defp get_prefix(:p2pkh, :testnet), do: "6F"
   defp get_prefix(:p2sh, :testnet), do: "C4"
+  defp get_prefix(_, _), do: ""
 
   defp get_address_type_from_prefix(0), do: :p2pkh
   defp get_address_type_from_prefix(5), do: :p2sh
   defp get_address_type_from_prefix(111), do: :p2pkh
   defp get_address_type_from_prefix(196), do: :p2sh
+  defp get_address_type_from_prefix(_), do: :unknown
 end
