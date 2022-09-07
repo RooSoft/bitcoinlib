5f6483b2a93183093716a0eafbf20c9be453ddfa
marc@roosoft.com <marc@roosoft.com>
2022-09-07 11:54:52 -0400

    upgraded dialyxir to 1.2

--- mix.exs
+++ mix.exs
@@ -72,7 +72,7 @@ defmodule BitcoinLib.MixProject do
   defp deps do
     [
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:dialyxir, "~> 0.5", only: [:dev]},
+      {:dialyxir, "~> 1.2", only: [:dev]},
       {:binary, "0.0.4"},
       {:exbase58, "~> 1.0"},
       {:pbkdf2_elixir, "~> 2.0"},
