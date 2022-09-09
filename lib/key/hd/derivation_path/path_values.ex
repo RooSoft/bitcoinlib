2e7e3201dac63424b191a8bda18b12562048689a
marc@roosoft.com <marc@roosoft.com>
2022-09-09 09:39:58 -0400

    documented and tested PathValues

--- lib/key/hd/derivation_path/path_values.ex
+++ lib/key/hd/derivation_path/path_values.ex
@@ -1,10 +1,28 @@
 defmodule BitcoinLib.Key.HD.DerivationPath.PathValues do
+  @moduledoc """
+  Single purpose module that's responsible to transform a list of integer into
+  a DerivationPath values structure
+  """
   @enforce_keys [:type]
 
   defstruct [:type, :purpose, :coin_type, :account, :change, :address_index]
 
   alias BitcoinLib.Key.HD.DerivationPath.PathValues
 
+  @doc """
+  Transform a list of integer into a DerivationPath values structure
+
+  ## Examples
+    iex> ["m", 0x80000000, 0x80000000, 0x80000005]
+    ...> |> BitcoinLib.Key.HD.DerivationPath.PathValues.from_list
+    %BitcoinLib.Key.HD.DerivationPath.PathValues{
+      type: "m",
+      purpose: 0x80000000,
+      coin_type: 0x80000000,
+      account: 0x80000005
+    }
+  """
+  @spec from_list(list()) :: %PathValues{}
   def from_list([type | remaining_values]) do
     {purpose, remaining_values} = extract_first_value(remaining_values)
     {coin_type, remaining_values} = extract_first_value(remaining_values)
---
+++ test/key/hd/derivation_path/path_values_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinLib.Key.HD.DerivationPath.PathValuesTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Key.HD.DerivationPath.PathValues
+end
