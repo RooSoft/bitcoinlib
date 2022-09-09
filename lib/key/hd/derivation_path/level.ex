ef2d3aac580185315995f449f364ddf2eef94eab
marc@roosoft.com <marc@roosoft.com>
2022-09-09 08:24:20 -0400

    moduledoc for DerivationPath.Level

--- lib/key/hd/derivation_path/level.ex
+++ lib/key/hd/derivation_path/level.ex
@@ -1,4 +1,10 @@
 defmodule BitcoinLib.Key.HD.DerivationPath.Level do
+  @moduledoc """
+  A derivation path is composed of levels of childrens, which have integer values and can be hardened
+
+  This is a simple structure defining this concept
+  """
+
   @enforce_keys [:hardened?, :value]
   defstruct hardened?: false, value: 0
 end
