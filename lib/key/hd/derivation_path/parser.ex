47b8873d798f4fd86e3bd209b5cbf7c7700de77a
marc@roosoft.com <marc@roosoft.com>
2022-09-09 09:57:56 -0400

    documented and tested BitcoinLib.Key.HD.DerivationPath.Parser

--- lib/key/hd/derivation_path/parser.ex
+++ lib/key/hd/derivation_path/parser.ex
@@ -13,6 +13,21 @@ defmodule BitcoinLib.Key.HD.DerivationPath.Parser do
   @doc """
   Single purpose function that's being called by DerivationPath.parse/1, returning a DerivationPath
   out of a string if the tuple starts by :ok
+
+  ## Examples
+    iex> {:ok, "m/44'/0'/0'/1/0"}
+    ...> |> BitcoinLib.Key.HD.DerivationPath.Parser.maybe_parse_valid_derivation_path
+    {
+      :ok,
+      %BitcoinLib.Key.HD.DerivationPath{
+        type: :private,
+        purpose: :bip44,
+        coin_type: :bitcoin,
+        account: %BitcoinLib.Key.HD.DerivationPath.Level{hardened?: true, value: 0},
+        change: :change_chain,
+        address_index: %BitcoinLib.Key.HD.DerivationPath.Level{hardened?: false, value: 0}
+      }
+    }
   """
   @spec maybe_parse_valid_derivation_path({:ok, binary()} | {:error, binary()}) ::
           {:ok, %DerivationPath{}} | {:error, binary()}
---
+++ test/key/hd/derivation_path/parser_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinLib.Key.HD.DerivationPath.ParserTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Key.HD.DerivationPath.Parser
+end
