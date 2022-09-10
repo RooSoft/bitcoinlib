391fb0174d1814008683f5a404a99993a1dd7122
marc@roosoft.com <marc@roosoft.com>
2022-09-10 13:20:11 -0400

    documented and doctested BitcoinLib.Key.PublicKey.Address

--- lib/key/public_key/address.ex
+++ lib/key/public_key/address.ex
@@ -1,7 +1,20 @@
 defmodule BitcoinLib.Key.PublicKey.Address do
+  @moduledoc """
+  Converts public keys into Bitcoin addresses of different formats
+  """
   alias BitcoinLib.Key.PublicKey
   alias BitcoinLib.Key.PublicKey.Address.{P2PKH, P2SH, Bech32}
 
+  @doc """
+  Turns a public key into an address of the specified format
+
+  ## Examples
+    iex> %BitcoinLib.Key.PublicKey{
+    ...>   key: <<0x0343B337DEC65A47B3362C9620A6E6FF39A1DDFA908ABAB1666C8A30A3F8A7CCCC::264>>
+    ...> }
+    ...> |> BitcoinLib.Key.PublicKey.Address.from_public_key(:bech32, :mainnet)
+    "bc1qa5gyew808tdta3wjh6qh3jvcglukjsnfg0qx4u"
+  """
   @spec from_public_key(%PublicKey{}, :p2pkh | :p2sh | :bech32, :mainnet | :testnet) ::
           binary()
   def from_public_key(%PublicKey{} = public_key, :p2pkh, :mainnet),
---
+++ test/key/public_key/address_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinLib.Key.PublicKey.AddressTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Key.PublicKey.Address
+end
