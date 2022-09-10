445f2c1df41b9c51ac930a9dcb1983fd0b6c3cca
marc@roosoft.com <marc@roosoft.com>
2022-09-10 13:48:34 -0400

    added a typespec for BitcoinLib.Key.PublicKey.Address.Bech32.from_public_key

--- lib/key/public_key/address/bech32.ex
+++ lib/key/public_key/address/bech32.ex
@@ -22,6 +22,7 @@ defmodule BitcoinLib.Key.PublicKey.Address.Bech32 do
     ...> } |> BitcoinLib.Key.PublicKey.Address.Bech32.from_public_key()
     "bc1qw508d6qejxtdg4y5r3zarvary0c5xw7kv8f3t4"
   """
+  @spec from_public_key(%PublicKey{}, :mainnet | :testnet) :: binary()
   def from_public_key(%PublicKey{key: key}, network \\ :mainnet) do
     key
     |> hash160
