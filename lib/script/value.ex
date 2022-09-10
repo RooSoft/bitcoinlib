443ebe0f511bdfd912e99c7b060235c7cefc63ea
marc@roosoft.com <marc@roosoft.com>
2022-09-10 14:07:27 -0400

    doctest and moduledoc for BitcoinLib.Script.Value

--- lib/script/value.ex
+++ lib/script/value.ex
@@ -1,6 +1,18 @@
 defmodule BitcoinLib.Script.Value do
+  @moduledoc """
+  Truthness evaluator for script execution outcomes
+  """
+
   @negative_zero 0x80
 
+  @doc """
+  Returns true if the value passed to it is considered as such
+
+  ## Examples
+    iex> BitcoinLib.Script.Value.is_true?(1)
+    true
+  """
+  @spec is_true?(list() | integer() | any()) :: boolean()
   def is_true?([]), do: false
   def is_true?(@negative_zero), do: false
   def is_true?(0), do: false
