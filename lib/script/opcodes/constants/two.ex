2ce4be9717a0852e5c128dfc3f5e324187a55137
marc@roosoft.com <marc@roosoft.com>
2022-09-12 12:33:23 -0400

    moduledoc for Constants.Two

--- lib/script/opcodes/constants/two.ex
+++ lib/script/opcodes/constants/two.ex
@@ -1,4 +1,15 @@
 defmodule BitcoinLib.Script.Opcodes.Constants.Two do
+  @moduledoc """
+  Based on https://en.bitcoin.it/wiki/Script
+
+  Word OP_2
+  Opcode 82
+  Hex 0x52
+  Input Nothing.
+  Output 2
+  Description The number 2 is pushed onto the stack.
+  """
+
   @behaviour BitcoinLib.Script.Opcode
 
   defstruct []
