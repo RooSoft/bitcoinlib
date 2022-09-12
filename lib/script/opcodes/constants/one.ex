4125f53a80157d045ae85a130a2eb5792de1f0bd
marc@roosoft.com <marc@roosoft.com>
2022-09-12 13:31:26 -0400

    documented, tested and spectyped functions of Constants.One

--- lib/script/opcodes/constants/one.ex
+++ lib/script/opcodes/constants/one.ex
@@ -18,15 +18,42 @@ defmodule BitcoinLib.Script.Opcodes.Constants.One do
 
   @value 0x51
 
+  @doc """
+  Returns 0x51
+
+  ## Examples
+    iex> BitcoinLib.Script.Opcodes.Constants.One.v()
+    0x51
+  """
+  @spec v() :: 0x51
   def v do
     @value
   end
 
+  @doc """
+  Returns <<0x51>>
+
+  ## Examples
+    iex> BitcoinLib.Script.Opcodes.Constants.One.encode()
+    <<0x51>>
+  """
+  @spec encode() :: <<_::8>>
   def encode() do
     <<@value::8>>
   end
 
-  def execute(%One{}, remaining) do
-    {:ok, remaining}
+  @doc """
+  The number 1 is pushed onto the stack.
+
+  ## Examples
+    iex> BitcoinLib.Script.Opcodes.Constants.One.execute(
+    ...>  %BitcoinLib.Script.Opcodes.Constants.One{},
+    ...>  [3]
+    ...> )
+    {:ok, [1,3]}
+  """
+  @spec execute(%One{}, list()) :: {:ok, list()}
+  def execute(%One{}, [remaining]) do
+    {:ok, [1, remaining]}
   end
 end
