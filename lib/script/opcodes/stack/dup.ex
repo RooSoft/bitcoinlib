14f4aed9ec6493758a0a1dff1d31d5ff6e3b4f5f
marc@roosoft.com <marc@roosoft.com>
2022-09-12 14:47:52 -0400

    documented, tested and spectyped functions of Stack.Dup

--- lib/script/opcodes/stack/dup.ex
+++ lib/script/opcodes/stack/dup.ex
@@ -14,16 +14,43 @@ defmodule BitcoinLib.Script.Opcodes.Stack.Dup do
 
   defstruct []
 
+  alias BitcoinLib.Script.Opcodes.Stack.Dup
+
   @value 0x76
 
+  @doc """
+  Returns 0x76
+
+  ## Examples
+    iex> BitcoinLib.Script.Opcodes.Stack.Dup.v()
+    0x76
+  """
+  @spec v() :: 0x76
   def v do
     @value
   end
 
+  @doc """
+  Returns <<0x76>>
+
+  ## Examples
+    iex> BitcoinLib.Script.Opcodes.Stack.Dup.encode()
+    <<0x76>>
+  """
   def encode() do
     <<@value::8>>
   end
 
+  @doc """
+  Duplicates the top stack item.
+
+  ## Examples
+    iex> stack = [4, 3]
+    ...> %BitcoinLib.Script.Opcodes.Stack.Dup{}
+    ...> |> BitcoinLib.Script.Opcodes.Stack.Dup.execute(stack)
+    {:ok, [4, 4, 3]}
+  """
+  @spec execute(%Dup{}, list()) :: {:ok, list()} | {:error, binary()}
   def execute(_opcode, []), do: {:error, "trying to execute OP_DUP on an empty stack"}
 
   def execute(_opcode, [first_element | _] = stack) do
