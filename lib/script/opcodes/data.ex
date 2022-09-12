a47f43298428e59b4e43925145022715f162a4e9
marc@roosoft.com <marc@roosoft.com>
2022-09-12 14:54:57 -0400

    documented, tested and spectyped functions of Opcodes.Data

--- lib/script/opcodes/data.ex
+++ lib/script/opcodes/data.ex
@@ -1,8 +1,20 @@
 defmodule BitcoinLib.Script.Opcodes.Data do
+  @moduledoc """
+  Adds between 1 and 75 bytes of data into the stack
+  """
   defstruct [:value]
 
   alias BitcoinLib.Script.Opcodes.Data
 
+  @doc """
+  Adds between 1 and 75 bytes of data into the stack
+
+  ## Examples
+    iex> %BitcoinLib.Script.Opcodes.Data{value: <<1, 2, 3, 4, 5>>}
+    ...> |> BitcoinLib.Script.Opcodes.Data.encode()
+    <<5, 1, 2, 3, 4, 5>>
+  """
+  @spec encode(%Data{}) :: bitstring()
   def encode(%Data{value: value}) do
     size_of_value = byte_size(value)
 
---
+++ test/script/opcodes/data_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinLib.Script.Opcodes.DataTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Script.Opcodes.Data
+end
