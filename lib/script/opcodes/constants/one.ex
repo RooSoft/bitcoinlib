75a6e74bc4867654cbeecba78a0f093d50a65283
marc@roosoft.com <marc@roosoft.com>
2022-09-12 12:32:19 -0400

    moduledoc for Constants.One

--- lib/script/opcodes/constants/one.ex
+++ lib/script/opcodes/constants/one.ex
@@ -1,4 +1,15 @@
 defmodule BitcoinLib.Script.Opcodes.Constants.One do
+  @moduledoc """
+  Based on https://en.bitcoin.it/wiki/Script
+
+  Word OP_1, OP_TRUE
+  Opcode 81
+  Hex 0x51
+  Input Nothing.
+  Output 1
+  Description The number 1 is pushed onto the stack.
+  """
+
   @behaviour BitcoinLib.Script.Opcode
 
   defstruct []
