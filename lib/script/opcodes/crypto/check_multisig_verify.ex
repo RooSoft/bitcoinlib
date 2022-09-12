4f46f8501855da3057d6fb2ed343cb3682339bc2
marc@roosoft.com <marc@roosoft.com>
2022-09-12 12:36:30 -0400

    moduledoc for Crypto.CheckMultiSigVerify

--- lib/script/opcodes/crypto/check_multisig_verify.ex
+++ lib/script/opcodes/crypto/check_multisig_verify.ex
@@ -1,4 +1,15 @@
 defmodule BitcoinLib.Script.Opcodes.Crypto.CheckMultiSigVerify do
+  @moduledoc """
+  Based on https://en.bitcoin.it/wiki/Script
+
+  Word OP_CHECKMULTISIGVERIFY
+  Opcode 175
+  Hex 0xaf
+  Input x sig1 sig2 ... <number of signatures> pub1 pub2 ... <number of public keys>
+  Output Nothing / fail
+  Description Same as OP_CHECKMULTISIG, but OP_VERIFY is executed afterward.
+  """
+
   @behaviour BitcoinLib.Script.Opcode
 
   defstruct []
