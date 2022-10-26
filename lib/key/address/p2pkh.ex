3f68aedd5998b7600a404a5f278d6fbc7a5111e7
marc@roosoft.com <marc@roosoft.com>
2022-10-26 09:29:17 -0400

    added from_public_key_hash to P2PKH

--- lib/key/address/p2pkh.ex
+++ lib/key/address/p2pkh.ex
@@ -8,6 +8,7 @@ defmodule BitcoinLib.Key.Address.P2PKH do
   """
 
   alias BitcoinLib.Key.{PublicKey, Address}
+  alias BitcoinLib.Crypto
 
   @doc """
   Converts an extended public key into a P2PKH address starting by 1
@@ -24,5 +25,43 @@ defmodule BitcoinLib.Key.Address.P2PKH do
     public_key
     |> PublicKey.hash()
     |> Address.from_public_key_hash(:p2pkh, network)
+  end
+
+  @doc """
+  Creates a P2PKH address, which is starting by 1, out of a script hash
+
+  ## Examples
+      iex> <<0xba27f99e007c7f605a8305e318c1abde3cd220ac::160>>
+      ...> |> BitcoinLib.Key.Address.P2PKH.from_public_key_hash(:testnet)
+      "mxVFsFW5N4mu1HPkxPttorvocvzeZ7KZyk"
+  """
+  @spec from_public_key_hash(<<_::160>>, :mainnet | :testnet) :: binary()
+  def from_public_key_hash(<<_::160>> = public_key_hash, network \\ :mainnet) do
+    public_key_hash
+    |> prepend_version_bytes(network)
+    |> append_checksum()
+    |> base58_encode
+    |> dbg
+  end
+
+  defp prepend_version_bytes(data, network) do
+    prefix = get_prefix(network)
+
+    <<prefix::bitstring, data::bitstring>>
+  end
+
+  defp get_prefix(:mainnet), do: <<0::8>>
+  defp get_prefix(:testnet), do: <<0x6F::8>>
+
+  defp append_checksum(data) do
+    checksum =
+      data
+      |> Crypto.checksum()
+
+    data <> checksum
+  end
+
+  defp base58_encode(data) do
+    Base58.encode(data)
   end
 end
