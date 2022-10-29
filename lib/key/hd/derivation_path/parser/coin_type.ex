94fd9ad92136275c8edc1a1b9adb047847b975e7
marc@roosoft.com <marc@roosoft.com>
2022-10-29 12:53:21 -0400

    moduledoc and doctests for DerivationPath.Parser.CoinType

--- lib/key/hd/derivation_path/parser/coin_type.ex
+++ lib/key/hd/derivation_path/parser/coin_type.ex
@@ -1,5 +1,11 @@
 defmodule BitcoinLib.Key.HD.DerivationPath.Parser.CoinType do
-  # https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#registered-coin-types
+  @moduledoc """
+  Determines which coin type is being used, either bitcoin mainnet or bitcoin testnet.
+
+  This is a hardened value.
+
+  See: https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki#registered-coin-types
+  """
   @bitcoin_coin_type "0'"
   @bitcoin_value 0
   @bitcoin_atom :bitcoin
@@ -8,6 +14,16 @@ defmodule BitcoinLib.Key.HD.DerivationPath.Parser.CoinType do
   @bitcoin_testnet_value 1
   @bitcoin_testnet_atom :bitcoin_testnet
 
+  @doc """
+  Converts a list of path levels managed up until the coin type, extracts
+  the coin type and returns the remaining levels
+
+  ## Examples
+      iex> ["1'", "1", "2", "3"]
+      ...> |> BitcoinLib.Key.HD.DerivationPath.Parser.CoinType.extract()
+      {:ok, :bitcoin_testnet, ["1", "2", "3"]}
+  """
+  @spec extract(list()) :: {:ok, nil, []} | {:ok, integer(), list()} | {:error, binary()}
   def extract([]), do: {:ok, nil, []}
   def extract([@bitcoin_coin_type | rest]), do: {:ok, @bitcoin_atom, rest}
   def extract([@bitcoin_testnet_coin_type | rest]), do: {:ok, @bitcoin_testnet_atom, rest}
