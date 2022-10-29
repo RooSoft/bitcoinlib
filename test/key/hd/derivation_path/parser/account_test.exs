c2b8914286dcbb0cfd771f54eb417c9885cd085d
marc@roosoft.com <marc@roosoft.com>
2022-10-29 13:55:34 -0400

    test derivation path extraction of an non-hardened account

--- test/key/hd/derivation_path/parser/account_test.exs
+++ test/key/hd/derivation_path/parser/account_test.exs
@@ -2,4 +2,16 @@ defmodule BitcoinLib.Key.HD.DerivationPath.Parser.AccountTest do
   use ExUnit.Case, async: true
 
   doctest BitcoinLib.Key.HD.DerivationPath.Parser.Account
+
+  alias BitcoinLib.Key.HD.DerivationPath.Parser.Account
+
+  test "derivation path extraction of an non-hardened account" do
+    derivation_path = ["2", "3", "4"]
+
+    {:error, message} =
+      derivation_path
+      |> Account.extract()
+
+    assert message == "account number must be a hardened value"
+  end
 end
