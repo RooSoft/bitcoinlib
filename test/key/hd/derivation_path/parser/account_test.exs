6cbff4e123d09b0a65c00fe6a1dbc435891f95e0
marc@roosoft.com <marc@roosoft.com>
2022-10-29 13:56:46 -0400

    test derivation path extraction of an empty account number

--- test/key/hd/derivation_path/parser/account_test.exs
+++ test/key/hd/derivation_path/parser/account_test.exs
@@ -14,4 +14,14 @@ defmodule BitcoinLib.Key.HD.DerivationPath.Parser.AccountTest do
 
     assert message == "account number must be a hardened value"
   end
+
+  test "derivation path extraction of an empty account number" do
+    derivation_path = []
+
+    result =
+      derivation_path
+      |> Account.extract()
+
+    assert {:ok, nil, []} == result
+  end
 end
