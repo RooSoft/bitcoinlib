53c502fccf7e9e2b7a025070b9fe3a2d8a18cd00
marc@roosoft.com <marc@roosoft.com>
2022-10-29 13:59:56 -0400

    test derivation path extraction of an invalid change chain

--- test/key/hd/derivation_path/parser/change_test.exs
+++ test/key/hd/derivation_path/parser/change_test.exs
@@ -5,7 +5,7 @@ defmodule BitcoinLib.Key.HD.DerivationPath.Parser.ChangeTest do
 
   alias BitcoinLib.Key.HD.DerivationPath.Parser.Change
 
-  test "derivation path extraction of a hardened change" do
+  test "derivation path extraction of a hardened change chain" do
     derivation_path = ["3'", "4"]
 
     {:error, message} =
@@ -13,5 +13,15 @@ defmodule BitcoinLib.Key.HD.DerivationPath.Parser.ChangeTest do
       |> Change.extract()
 
     assert message == "change chain must NOT be a hardened value"
+  end
+
+  test "derivation path extraction of an invalid change chain" do
+    derivation_path = ["3", "4"]
+
+    {:error, message} =
+      derivation_path
+      |> Change.extract()
+
+    assert message =~ "is not a valid change chain"
   end
 end
