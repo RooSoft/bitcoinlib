3338408c3c6b7f581b44e4a349912793ad6d2720
marc@roosoft.com <marc@roosoft.com>
2022-11-01 09:27:32 -0400

    added BitcoinLib.Bitstring.reverse, that converts from big endian to little endian and back

---
+++ lib/crypto/bitstring.ex
@@ -0,0 +1,18 @@
+defmodule BitcoinLib.Bitstring do
+  @doc """
+  Takes any bistring and returns bytes in the reverse order, such as going from
+  little endian to big endian or the opposite
+
+  ## Examples
+      iex> <<0xb62e9d36389427d39e5d438a05045c23d1938e4242661c5fe2ad87c46337b091::256>>
+      ...> |> BitcoinLib.Bitstring.reverse()
+      <<0x91b03763c487ade25f1c6642428e93d1235c04058a435d9ed3279438369d2eb6::256>>
+  """
+  @spec reverse(bitstring()) :: bitstring()
+  def reverse(bitstring) do
+    bitstring
+    |> :binary.bin_to_list()
+    |> Enum.reverse()
+    |> :binary.list_to_bin()
+  end
+end
---
+++ test/crypto/bitstring_test.exs
@@ -0,0 +1,5 @@
+defmodule BitcoinLib.BitstringTest do
+  use ExUnit.Case, async: true
+
+  doctest BitcoinLib.Bitstring
+end
