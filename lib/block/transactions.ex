8903fc6705670511515d1578b9918e656bb3b939
marc@roosoft.com <marc@roosoft.com>
2022-11-15 14:40:39 -0500

    validate expected number of transactions in a block

--- lib/block/transactions.ex
+++ lib/block/transactions.ex
@@ -10,21 +10,33 @@ defmodule BitcoinLib.Block.Transactions do
   def decode(transactions_data) do
     %CompactInteger{
       remaining: remaining,
-      value: _transaction_count
+      value: transaction_count
     } = CompactInteger.extract_from(transactions_data)
 
-    with {:ok, transactions} <- scan(remaining) do
+    with {:ok, transactions} <- scan(transaction_count, remaining) do
       {:ok, transactions}
     else
       {:error, message} -> {:error, message}
     end
   end
 
-  defp scan(remaininig) do
+  defp scan(expected_transaction_count, remaininig) do
     with {:ok, transaction} <- Transaction.decode(remaininig) do
-      {:ok, [transaction]}
+      transactions = [transaction]
+
+      validate(expected_transaction_count, transactions)
     else
       {:error, message} -> {:error, message}
+    end
+  end
+
+  defp validate(expected_transaction_count, transactions) do
+    case Enum.count(transactions) do
+      ^expected_transaction_count ->
+        {:ok, transactions}
+
+      transaction_count ->
+        {:error, "got #{transaction_count} transactions, expected #{expected_transaction_count}"}
     end
   end
 end
