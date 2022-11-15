0736c1ebd0213555dfa0e305767d5fbafb61f8be
marc@roosoft.com <marc@roosoft.com>
2022-11-15 10:00:20 -0500

    typespecs and doctests for P2WPKH

--- lib/address/p2wpkh.ex
+++ lib/address/p2wpkh.ex
@@ -9,10 +9,28 @@ defmodule BitcoinLib.Address.P2WPKH do
 
   alias BitcoinLib.Address.Bech32
 
+  @doc """
+  Creates a P2WPKH address out of a key hash
+
+  ## Examples
+      iex> <<0x13BFFF2D6DD02B8837F156C6F9FE0EA7363DF795::160>>
+      ...> |> BitcoinLib.Address.P2WPKH.from_key_hash()
+      "bc1qzwll7ttd6q4csdl32mr0nlsw5umrmau4es49qe"
+  """
+  @spec from_key_hash(<<_::160>>, :mainnet | :testnet) :: binary()
   def from_key_hash(key_hash, network \\ :mainnet) do
     from_script_pub_key(<<0x0014::16, key_hash::bitstring-160>>, network)
   end
 
+  @doc """
+  Creates a P2WPKH address out of a script pub key
+
+  ## Examples
+      iex> <<0x001413BFFF2D6DD02B8837F156C6F9FE0EA7363DF795::176>>
+      ...> |> BitcoinLib.Address.P2WPKH.from_script_pub_key()
+      "bc1qzwll7ttd6q4csdl32mr0nlsw5umrmau4es49qe"
+  """
+  @spec from_script_pub_key(<<_::176>>, :mainnet | :testnet) :: binary()
   def from_script_pub_key(
         <<0x0014::16, _script_hash::bitstring-160>> = script_pub_key,
         network \\ :mainnet
