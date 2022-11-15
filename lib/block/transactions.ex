f9dafc7b6c21372789920274778f27ac528d0e42
marc@roosoft.com <marc@roosoft.com>
2022-11-15 14:22:08 -0500

    manage error message while parsing block headers

---
+++ lib/block/transactions.ex
@@ -0,0 +1,9 @@
+defmodule BitcoinLib.Block.Transactions do
+  @moduledoc """
+  Converts a list of bytes into a list of transactions
+  """
+
+  def parse(_transactions_data) do
+    {:ok, []}
+  end
+end
