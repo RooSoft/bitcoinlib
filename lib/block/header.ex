8cfc46687a4d77491aa9eaaf759e5d3845c81298
marc@roosoft.com <marc@roosoft.com>
2022-11-15 14:19:57 -0500

    add error checking for block header parsing

--- lib/block/header.ex
+++ lib/block/header.ex
@@ -18,13 +18,16 @@ defmodule BitcoinLib.Block.Header do
       iex> "0100000000000000000000000000000000000000000000000000000000000000000000003ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a29ab5f49ffff001d1dac2b7c"
       ...> |> Binary.from_hex()
       ...> |> BitcoinLib.Block.Header.parse()
-      %BitcoinLib.Block.Header{
-        version: 1,
-        previous_block_hash: <<0::256>>,
-        merkle_root_hash: <<0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b::256>>,
-        time: ~U[2009-01-03 18:15:05Z],
-        bits: 486604799,
-        nonce: 2083236893
+      {
+        :ok,
+        %BitcoinLib.Block.Header{
+          version: 1,
+          previous_block_hash: <<0::256>>,
+          merkle_root_hash: <<0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b::256>>,
+          time: ~U[2009-01-03 18:15:05Z],
+          bits: 486604799,
+          nonce: 2083236893
+        }
       }
   """
   @spec parse(<<_::640>>) :: {:ok, %Header{}} | {:error, binary()}
@@ -36,13 +39,18 @@ defmodule BitcoinLib.Block.Header do
         bits::little-32,
         nonce::little-32
       >>) do
-    %Header{
-      version: version,
-      previous_block_hash: previous_block_hash |> Bitstring.reverse(),
-      merkle_root_hash: merkle_root_hash |> Bitstring.reverse(),
-      time: DateTime.from_unix!(time),
-      bits: bits,
-      nonce: nonce
+    {
+      :ok,
+      %Header{
+        version: version,
+        previous_block_hash: previous_block_hash |> Bitstring.reverse(),
+        merkle_root_hash: merkle_root_hash |> Bitstring.reverse(),
+        time: DateTime.from_unix!(time),
+        bits: bits,
+        nonce: nonce
+      }
     }
   end
+
+  def parse(_), do: {:error, "invalid block"}
 end
--- test/block/header_test.exs
+++ test/block/header_test.exs
@@ -2,4 +2,14 @@ defmodule BitcoinLib.Block.HeaderTest do
   use ExUnit.Case, async: true
 
   doctest BitcoinLib.Block.Header
+
+  alias BitcoinLib.Block.Header
+
+  test "parsing an invalid block" do
+    invalid_block_data = <<0>>
+
+    {:error, message} = Header.parse(invalid_block_data)
+
+    assert "invalid block" == message
+  end
 end
