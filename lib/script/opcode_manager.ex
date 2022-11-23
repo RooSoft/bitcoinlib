fc0da6e9a3c4a85bf621cdf089e4411fa0dbad73
marc@roosoft.com <marc@roosoft.com>
2022-11-23 15:29:37 -0500

    added the OP_2OVER opcode

--- lib/script/opcode_manager.ex
+++ lib/script/opcode_manager.ex
@@ -28,6 +28,7 @@ defmodule BitcoinLib.Script.OpcodeManager do
   @drop Stack.Drop.v()
   @dup Stack.Dup.v()
   @rot Stack.Rot.v()
+  @two_over Stack.TwoOver.v()
   @two_swap Stack.TwoSwap.v()
   @equal BitwiseLogic.Equal.v()
   @equal_verify BitwiseLogic.EqualVerify.v()
@@ -83,6 +84,10 @@ defmodule BitcoinLib.Script.OpcodeManager do
 
   def encode_opcode(%Stack.Rot{}) do
     Stack.Rot.encode()
+  end
+
+  def encode_opcode(%Stack.TwoOver{}) do
+    Stack.TwoOver.encode()
   end
 
   def encode_opcode(%Stack.TwoSwap{}) do
@@ -206,6 +211,10 @@ defmodule BitcoinLib.Script.OpcodeManager do
 
   def extract_from_script(<<@dup::8, remaining::bitstring>>, _whole_script) do
     {:opcode, %Stack.Dup{}, remaining}
+  end
+
+  def extract_from_script(<<@two_over::8, remaining::bitstring>>, _whole_script) do
+    {:opcode, %Stack.TwoOver{}, remaining}
   end
 
   def extract_from_script(<<@two_swap::8, remaining::bitstring>>, _whole_script) do
---
+++ lib/script/opcodes/stack/two_over.ex
@@ -0,0 +1,62 @@
+defmodule BitcoinLib.Script.Opcodes.Stack.TwoOver do
+  @moduledoc """
+  Based on https://en.bitcoin.it/wiki/Script
+
+  Word OP_2OVER
+  Opcode 112
+  Hex 0x70
+  Input x1 x2 x3 x4
+  Output x1 x2 x3 x4 x1 x2
+  Copies the pair of items two spaces back in the stack to the front.
+  """
+
+  @behaviour BitcoinLib.Script.Opcode
+
+  defstruct []
+
+  alias BitcoinLib.Script.Opcodes.Stack.TwoOver
+
+  @value 0x70
+
+  @doc """
+  Returns 0x70
+
+  ## Examples
+      iex> BitcoinLib.Script.Opcodes.Stack.TwoOver.v()
+      0x70
+  """
+  @spec v() :: 0x70
+  def v do
+    @value
+  end
+
+  @doc """
+  Returns <<0x70>>
+
+  ## Examples
+      iex> BitcoinLib.Script.Opcodes.Stack.TwoOver.encode()
+      <<0x70>>
+  """
+  def encode() do
+    <<@value::8>>
+  end
+
+  @doc """
+  Copies the pair of items two spaces back in the stack to the front.
+
+  ## Examples
+      iex> stack = [1, 2, 3, 4, 5]
+      ...> %BitcoinLib.Script.Opcodes.Stack.TwoOver{}
+      ...> |> BitcoinLib.Script.Opcodes.Stack.TwoOver.execute(stack)
+      {:ok, [1, 2, 3, 4, 1, 2, 5]}
+  """
+  @spec execute(%TwoOver{}, list()) :: {:ok, list()} | {:error, binary()}
+  def execute(_opcode, []), do: {:error, "trying to execute OP_DUP on an empty stack"}
+  def execute(_opcode, [_]), do: {:error, "trying to execute OP_DUP on a 1 element stack"}
+  def execute(_opcode, [_, _]), do: {:error, "trying to execute OP_DUP on a 2 elements stack"}
+  def execute(_opcode, [_, _, _]), do: {:error, "trying to execute OP_DUP on a 3 elements stack"}
+
+  def execute(_opcode, [x1, x2, x3, x4] ++ rest) do
+    {:ok, [x1, x2, x3, x4, x1, x2] ++ rest}
+  end
+end
